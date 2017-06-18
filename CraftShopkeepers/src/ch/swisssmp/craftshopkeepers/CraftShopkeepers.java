b2a876b5e746e7a0e2c555d9c173abb0a9a88f24
BuildTools <star.etoa@hotmail.com>
2017-06-19 01:52:52 +0200

    CraftShopkeepers updated

--- CraftShopkeepers/src/ch/swisssmp/craftshopkeepers/CraftShopkeepers.java
+++ CraftShopkeepers/src/ch/swisssmp/craftshopkeepers/CraftShopkeepers.java
@@ -3,22 +3,14 @@ package ch.swisssmp.craftshopkeepers;
 import java.util.Arrays;
 import java.util.logging.Logger;
 
-import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.Server;
-import org.bukkit.entity.ArmorStand;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -31,12 +23,15 @@ public class CraftShopkeepers extends JavaPlugin implements Listener{
 	public ItemStack shopkeeper;
 	public Server server;
 	public ShapedRecipe shopkeeperRecipe;
+	protected NamespacedKey namespacedKey;
+	
 	public void onEnable() {
 		PluginDescriptionFile pdfFile = getDescription();
 		logger = Logger.getLogger("Minecraft");
 		logger.info(pdfFile.getName() + " has been enabled (Version: " + pdfFile.getVersion() + ")");
 		
 		server = getServer();
+		namespacedKey = new NamespacedKey(this, "Marktplatz");
 		
 		server.getPluginManager().registerEvents(this, this);
 		createRecipe();
@@ -55,7 +50,7 @@ public class CraftShopkeepers extends JavaPlugin implements Listener{
 		meta.setDisplayName("Händler");
 		meta.setLore(Arrays.asList("Platziere diesen Händler", "auf einem Marktplatz,", "um einen Stand zu eröffnen."));
 		shopkeeper.setItemMeta(meta);
-		shopkeeperRecipe = new ShapedRecipe(shopkeeper);
+		shopkeeperRecipe = new ShapedRecipe(namespacedKey, shopkeeper);
 		shopkeeperRecipe.shape("eee","drd","eee");
 		shopkeeperRecipe.setIngredient('d', Material.DIAMOND);
 		shopkeeperRecipe.setIngredient('r', Material.ROTTEN_FLESH);
@@ -63,8 +58,9 @@ public class CraftShopkeepers extends JavaPlugin implements Listener{
 		server.addRecipe(shopkeeperRecipe);
 		logger.info("Custom Recipe has been created");
 	}
-    @EventHandler
+    @EventHandler(ignoreCancelled=true)
     public void craftItem(PrepareItemCraftEvent event) {
+    	if(event.getRecipe()==null) return;
     	ItemStack result = event.getRecipe().getResult();
     	if(!result.hasItemMeta())
     		return;
@@ -80,42 +76,4 @@ public class CraftShopkeepers extends JavaPlugin implements Listener{
             }
     	}
     }
-    /*@EventHandler(ignoreCancelled=true)
-    private void onPlayerChat(PlayerInteractEvent event){
-    	if(event.getAction()!=Action.RIGHT_CLICK_BLOCK) return;
-    	if(event.getItem().getType()==Material.MONSTER_EGG){
-    		Player player = event.getPlayer();
-    		Location location = event.getClickedBlock().getRelative(event.getBlockFace()).getLocation();
-	        ArmorStand vehicle = (ArmorStand) player.getWorld().spawn(new Location(location.getWorld(), location.getX(), location.getY()-1.5, location.getZ()), ArmorStand.class);
-	        Villager villager = (Villager) player.getWorld().spawn(location, Villager.class);
-	        villager.setCustomName("Händler");
-	        villager.setCustomNameVisible(true);
-	        villager.setInvulnerable(true);
-	        villager.setCollidable(false);
-	        vehicle.setVisible(false);
-	        vehicle.setGravity(false);
-	        vehicle.setCustomName("shop_1");
-	        vehicle.setCustomNameVisible(false);
-	        vehicle.setPassenger(villager);
-	        event.setCancelled(true);
-    	}
-    }
-    @EventHandler(ignoreCancelled=true)
-    private void onPlayerInteractEntity(PlayerInteractEntityEvent event){
-    	Entity interacted = event.getRightClicked();
-    	if(interacted.getType()!=EntityType.VILLAGER) return;
-    	Entity vehicle = interacted.getVehicle();
-    	if(vehicle==null) return;
-    	if(vehicle.getType()!=EntityType.ARMOR_STAND) return;
-    	event.getPlayer().sendMessage(vehicle.getCustomName());
-    }
-    @EventHandler(ignoreCancelled=true)
-    private void onEntityDeath(EntityDeathEvent event){
-    	Entity dead = event.getEntity();
-    	if(dead.getType()!=EntityType.VILLAGER) return;
-    	Entity vehicle = dead.getVehicle();
-    	if(vehicle==null) return;
-    	if(vehicle.getType()!=EntityType.ARMOR_STAND) return;
-    	vehicle.remove();
-    }*/
 }
