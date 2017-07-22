62e9a17915675ee7c8b39583ca5884adffac164d
BuildTools <star.etoa@hotmail.com>
2017-07-22 23:10:39 +0200

    Akroma Tempel als Ersatzspawn

--- SpawnManager/src/ch/swisssmp/spawnmanager/PlayerCommand.java
+++ SpawnManager/src/ch/swisssmp/spawnmanager/PlayerCommand.java
@@ -4,6 +4,7 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -20,20 +21,45 @@ public class PlayerCommand implements CommandExecutor{
 			return true;
 		}
 		Player player = (Player) sender;
-		try {
-			YamlConfiguration yamlConfiguration;
-			yamlConfiguration = DataSource.getYamlResponse("players/spawn.php", new String[]{
-					"player="+player.getUniqueId().toString(),
-					"world="+URLEncoder.encode(player.getWorld().getName(), "utf-8")
-			});
-			if(yamlConfiguration==null) return true;
-			Location location = yamlConfiguration.getLocation("spawnpoint");
-			if(location!=null){
-				player.teleport(location);
+		switch(label){
+		case "spawn":{
+			try {
+				YamlConfiguration yamlConfiguration;
+				yamlConfiguration = DataSource.getYamlResponse("players/spawn.php", new String[]{
+						"player="+player.getUniqueId().toString(),
+						"world="+URLEncoder.encode(player.getWorld().getName(), "utf-8"),
+						"flag=worldspawn"
+				});
+				if(yamlConfiguration==null) return true;
+				Location location = yamlConfiguration.getLocation("spawnpoint");
+				if(location!=null){
+					player.teleport(location);
+				}
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			break;
+		}
+		case "settemplespawn":{
+			if(args==null || args.length<1) return false;
+			World world = player.getWorld();
+			Location location = player.getLocation();
+			try{
+				String response = DataSource.getResponse("addons/set_temple_spawn.php", new String[]{
+					"city="+URLEncoder.encode(args[0], "utf-8"),
+					"world="+URLEncoder.encode(world.getName(), "utf-8"),
+					"x="+(int)Math.round(location.getX()),
+					"y="+(int)Math.round(location.getY()),
+					"z="+(int)Math.round(location.getZ()),
+				});
+				player.sendMessage(response);
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+		}
 		}
 		return true;
 	}
--- SpawnManager/src/ch/swisssmp/spawnmanager/SpawnManager.java
+++ SpawnManager/src/ch/swisssmp/spawnmanager/SpawnManager.java
@@ -3,18 +3,22 @@ package ch.swisssmp.spawnmanager;
 import java.io.File;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.Random;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
 
+import ch.swisssmp.utils.SwissSMPler;
 import ch.swisssmp.utils.YamlConfiguration;
 import ch.swisssmp.webcore.DataSource;
 
@@ -24,6 +28,7 @@ public class SpawnManager extends JavaPlugin implements Listener{
 	protected static File dataFolder;
 	protected static SpawnManager plugin;
 	protected static boolean debug;
+	private static Random random = new Random();
 	
 	@Override
 	public void onEnable() {
@@ -33,7 +38,9 @@ public class SpawnManager extends JavaPlugin implements Listener{
 		logger.info(pdfFile.getName() + " has been enabled (Version: " + pdfFile.getVersion() + ")");
 		
 		Bukkit.getPluginManager().registerEvents(this, this);
-		this.getCommand("spawn").setExecutor(new PlayerCommand());
+		PlayerCommand playerCommand = new PlayerCommand();
+		this.getCommand("spawn").setExecutor(playerCommand);
+		this.getCommand("settemplespawn").setExecutor(playerCommand);
 	}
 
 	@Override
@@ -44,21 +51,50 @@ public class SpawnManager extends JavaPlugin implements Listener{
 	}
 	
 	@EventHandler(ignoreCancelled=true)
-	private void onPlayerDeath(PlayerDeathEvent event){
-		Player player = event.getEntity();
-		if(player.getBedSpawnLocation()!=null){
+	private void onPlayerRespawn(PlayerRespawnEvent event){
+		Player player = event.getPlayer();
+		if(event.isBedSpawn()){
 			return;
 		}
 		try {
 			YamlConfiguration yamlConfiguration;
 			yamlConfiguration = DataSource.getYamlResponse("players/spawn.php", new String[]{
 					"player="+player.getUniqueId().toString(),
-					"world="+URLEncoder.encode(player.getWorld().getName(), "utf-8")
+					"x="+(int)Math.round(player.getLocation().getX()),
+					"y="+(int)Math.round(player.getLocation().getY()),
+					"z="+(int)Math.round(player.getLocation().getZ()),
+					"world="+URLEncoder.encode(player.getWorld().getName(), "utf-8"),
+					"flag=respawn"
 			});
 			if(yamlConfiguration==null) return;
-			Location location = yamlConfiguration.getLocation("spawnpoint");
-			if(location!=null){
-				player.setBedSpawnLocation(location, true);
+			if(yamlConfiguration.contains("spawnpoint")){
+				Location location = yamlConfiguration.getLocation("spawnpoint");
+				if(location!=null){
+					event.setRespawnLocation(location);
+				}
+			}
+			if(yamlConfiguration.contains("message")){
+				player.sendMessage(yamlConfiguration.getString("message"));
+			}
+			if(yamlConfiguration.contains("actionbar") || yamlConfiguration.contains("effect") || yamlConfiguration.contains("sound")){
+				Bukkit.getScheduler().runTaskLater(SpawnManager.plugin, new Runnable(){
+					public void run(){
+						if(yamlConfiguration.contains("actionbar")){
+							SwissSMPler swisssmpler = SwissSMPler.get(player);
+							swisssmpler.sendActionBar(yamlConfiguration.getString("actionbar"));
+						}
+						if(yamlConfiguration.contains("effect")){
+							PotionEffect potionEffect = yamlConfiguration.getPotionEffect("effect");
+							if(potionEffect!=null) player.addPotionEffect(potionEffect);
+						}
+						if(yamlConfiguration.contains("sound")){
+							Sound sound = Sound.valueOf(yamlConfiguration.getString("sound"));
+							if(sound!=null){
+								player.getWorld().playSound(player.getLocation(), sound, 5f, 0.8f+random.nextFloat()*0.4f);
+							}
+						}
+					}
+				}, 1l);
 			}
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
