b434386adcf6627f95552d47ec925ef990c5a641
BuildTools <star.etoa@hotmail.com>
2018-02-09 19:12:25 +0100

    CamStudio geladen und angepasst

---
+++ CamStudio/eu/crushedpixel/camerastudio/CamCommand.java
@@ -0,0 +1,347 @@
+package eu.crushedpixel.camerastudio;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.permissions.PermissionAttachmentInfo;
+
+import ch.swisssmp.utils.ConfigurationSection;
+import ch.swisssmp.utils.YamlConfiguration;
+import ch.swisssmp.webcore.DataSource;
+
+public class CamCommand implements CommandExecutor {
+
+	public static HashMap<UUID, List<Location>> points = new HashMap<UUID, List<Location>>();
+	private static String prefix = CameraStudio.prefix;
+	final static int previewTime = CameraStudio.instance.getConfig().getInt("preview-time");
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (!(sender instanceof Player)) {
+			sender.sendMessage("Kann nur ingame ausgeführt werden.");
+			return true;
+		}
+		final Player player = (Player) sender;
+		if (args.length == 0) {
+			player.sendMessage(
+					prefix + ChatColor.RED + "Type " + ChatColor.WHITE + "/cam help" + ChatColor.RED + " for details");
+			return true;
+		}
+
+		if (!CameraStudio.instance.getConfig().getStringList("allowed-gamemodes")
+				.contains(player.getGameMode().toString()) && !player.hasPermission("camerastudio.override-gamemode")) {
+			player.sendMessage(prefix + ChatColor.RED + "Du kannst diesen Befehl nicht in diesem GameMode verwenden.");
+			return true;
+		}
+
+		String subcmd = args[0];
+
+		String[] newArgs = new String[args.length - 1];
+		for (int p = 1; p < args.length; p++) {
+			newArgs[(p - 1)] = args[p];
+		}
+
+		args = newArgs;
+
+		switch(subcmd.toLowerCase()){
+		case "p":{
+			if(!player.hasPermission("camerastudio.point")){
+				return true;
+			}
+			List<Location> locs = (List<Location>) CamCommand.points.get(player.getUniqueId());
+			if (locs == null) {
+				locs = new ArrayList<Location>();
+			}
+
+			int maxPoints = new Integer(CameraStudio.instance.getConfig().getInt("maximum-points"));
+
+			if (player.isOp()) {
+				maxPoints = Integer.MAX_VALUE;
+			} else {
+				for (PermissionAttachmentInfo perm : player.getEffectivePermissions()) {
+					String permS = perm.getPermission();
+					if (permS == "*") {
+						maxPoints = Integer.MAX_VALUE;
+						break;
+					}
+					if (permS.startsWith("camerastudio.point.")) {
+						String substring = permS.substring(19);
+						if (substring == "*") {
+							maxPoints = Integer.MAX_VALUE;
+							break;
+						} else {
+							try {
+								maxPoints = Integer.parseInt(substring);
+							} catch (NumberFormatException e) {
+								player.sendMessage(prefix + ChatColor.RED
+										+ "Berechtigungen nicht korrekt definiert. Bitte benachrichtige einen Ressortleiter.");
+							}
+							break;
+						}
+					}
+				}
+			}
+
+			if (locs.size() >= maxPoints) {
+				player.sendMessage(
+						prefix + ChatColor.RED + "Du hast bereits " + maxPoints + " Punkte gesetzt.");
+				return true;
+			}
+
+			locs.add(player.getLocation());
+			CamCommand.points.put(player.getUniqueId(), locs);
+
+			player.sendMessage(prefix + "Point " + locs.size() + " has been set");
+
+			return true;
+		}
+		case "r":{
+			if(!player.hasPermission("camerastudio.remove")){
+				return true;
+			}
+			List<Location> locs = (List<Location>) CamCommand.points.get(player.getUniqueId());
+			if (locs == null) {
+				locs = new ArrayList<Location>();
+			}
+
+			if (args.length == 0) {
+				if (locs.size() > 0) {
+					locs.remove(locs.size() - 1);
+					player.sendMessage(prefix + "Punkt " + (locs.size() + 1) + " entfernt.");
+				} else {
+					player.sendMessage(prefix + ChatColor.RED + "Du hast noch keine Punkte gesetzt.");
+					return true;
+				}
+			} else if (args.length == 1) {
+				try {
+					int pos = Integer.valueOf(args[0]).intValue();
+					if (locs.size() >= pos) {
+						locs.remove(pos - 1);
+						player.sendMessage(prefix + "Punkt " + pos + " entfernt.");
+					} else {
+						if (locs.size() == 1) {
+							player.sendMessage(prefix + ChatColor.RED + "Du hast nur einen Punkt gesetzt.");
+							return true;
+						} else {
+							player.sendMessage(prefix + ChatColor.RED + "Du hast nur " + locs.size() + " Punkte gesetzt.");
+							return true;
+						}
+					}
+				} catch (Exception e) {
+					player.sendMessage(prefix + ChatColor.RED + args[0] + " ist keine gültige Zahl.");
+					return true;
+				}
+			}
+
+			CamCommand.points.put(player.getUniqueId(), locs);
+			return true;
+		}
+		case "list":{
+			if(!player.hasPermission("camerastudio.list")){
+				return true;
+			}
+			List<Location> locs = (List<Location>) CamCommand.points.get(player.getUniqueId());
+			if ((locs == null) || (locs.size() == 0)) {
+				player.sendMessage(prefix + ChatColor.RED + "Du hast noch keine Punkte gesetzt.");
+				return true;
+			}
+
+			int i = 1;
+			for (Location loc : locs) {
+				player.sendMessage(prefix + "Point " + i + ": " + CameraStudio.round(loc.getX(), 1) + ", "
+						+ CameraStudio.round(loc.getY(), 1) + ", " + CameraStudio.round(loc.getZ(), 1) + " ("
+						+ CameraStudio.round(loc.getYaw(), 1) + ", " + CameraStudio.round(loc.getPitch(), 1) + ")");
+				i++;
+			}
+			return true;
+		}
+		case "reset":{
+			if(!player.hasPermission("camerastudio.reset")){
+				return true;
+			}
+			// Made an error, replaced it with the line below:
+			// this.points.put(player, new ArrayList<Object>());
+			CamCommand.points.remove(player.getUniqueId());
+			player.sendMessage(prefix + "Alle Punkte entfernt.");
+			return true;
+		}
+		case "reload":{
+			if(!player.hasPermission("camerastudio.admin")){
+				return true;
+			}
+			CameraStudio.instance.reloadConfig();
+			sender.sendMessage(prefix + ChatColor.YELLOW + "Konfiguration neu geladen!");
+			return true;
+		}
+		case "goto":{
+			if(!player.hasPermission("camerastudio.goto")){
+				return true;
+			}
+			if (args.length == 1) {
+				try {
+					int pos = Integer.valueOf(args[0]).intValue();
+					List<Location> locs = (List<Location>) CamCommand.points.get(player.getUniqueId());
+					if ((locs != null) && (locs.size() >= pos)) {
+						player.teleport((Location) locs.get(pos - 1));
+						player.sendMessage(prefix + "Zu Punkt " + pos + " teleportiert.");
+						return true; // This was "break label1152;"
+					}
+					if (locs == null) {
+						player.sendMessage(prefix + ChatColor.RED + "Du hast noch keine Punkte gesetzt.");
+						return true;
+					}
+					player.sendMessage(prefix + ChatColor.RED + "Du hast nur " + locs.size() + " Punkte gesetzt");
+					return true;
+				} catch (Exception e) {
+					player.sendMessage(prefix + ChatColor.RED + args[0] + " ist keine Zahl.");
+					return true;
+				}
+			}
+			player.sendMessage(prefix + ChatColor.RED + "Du musst einen Zielpunkt angeben.");
+
+			// Says it wasn't referenced, I just removed it:
+			// label1152:
+			// return true;
+			return true;
+		}
+		case "stop":{
+			if(!player.hasPermission("camerastudio.stop")){
+				return true;
+			}
+			CameraStudio.stop(player.getUniqueId());
+			player.sendMessage(prefix + "Kamerafahrt gestoppt.");
+			return true;
+		}
+		case "help":{
+			if(!player.hasPermission("camerastudio.help")){
+				return true;
+			}
+			if (args.length == 0) {
+				player.performCommand("help CPCameraStudioReborn");
+				return true;
+			} else {
+				if (args.length>0) {
+					try {
+						player.performCommand("help CPCameraStudioReborn " + Integer.parseInt(args[0]));
+						return true;
+					} catch (NumberFormatException e) {
+						player.sendMessage(prefix + ChatColor.YELLOW + args[0] + ChatColor.RED + "is not a number!");
+						return true;
+					}
+				}
+			}
+			return true;
+		}
+		case "start":{
+			if(!player.hasPermission("camerastudio.start")){
+				return true;
+			}
+
+			List<Location> listOfLocs = new ArrayList<Location>();
+			if (CamCommand.points.get(player.getUniqueId()) != null)
+				listOfLocs.addAll(CamCommand.points.get(player.getUniqueId()));
+			Player currentPlayer = player;
+
+			if (CameraStudio.isTravelling(currentPlayer.getUniqueId())) {
+				currentPlayer.sendMessage(prefix + ChatColor.RED + "Du bewegst dich bereits auf einem Pfad.");
+				return true;
+			}
+
+			if (listOfLocs.isEmpty()) {
+				player.sendMessage(prefix + ChatColor.RED + "Nicht genügend Punkte gesetzt.");
+				return true;
+			}
+			try {
+				int time = previewTime * (listOfLocs.size() - 1);
+				if (subcmd.equalsIgnoreCase("start")) {
+					time = Integer.parseInt(args[0])*20;
+				}
+				CameraStudio.travel(currentPlayer, listOfLocs, time,
+						prefix + ChatColor.RED + "Ein Fehler ist aufgetreten.",
+						prefix + "Kamerafahrt beendet.");
+			} catch (Exception e) {
+				player.sendMessage(prefix + ChatColor.RED + "Du musst eine Dauer (in Sekunden) für die Kamerafahrt angeben."
+						+ ChatColor.YELLOW + "Beispiel: /cam start 60");
+			}
+			return true;
+		}
+		case "save":{
+			if(!player.hasPermission("camerastudio.save")){
+				return true;
+			}
+			if(args.length<1){
+				return false;
+			}
+			String name = args[0];
+			if (points.get(player.getUniqueId()) != null) {
+
+				List<String> locationLines = new ArrayList<String>();
+				int index = 0;
+				for (Location loc : points.get(player.getUniqueId())) {
+					locationLines.add("points["+index+"][x]="+loc.getX());
+					locationLines.add("points["+index+"][y]="+loc.getY());
+					locationLines.add("points["+index+"][z]="+loc.getZ());
+					locationLines.add("points["+index+"][pitch]="+loc.getPitch());
+					locationLines.add("points["+index+"][yaw]="+loc.getYaw());
+					index++;
+				}
+
+				try {
+					DataSource.getResponse("camera_studio/save_path.php", new String[]{
+						"name="+URLEncoder.encode(name,"utf-8"),
+						String.join("&", locationLines)
+					});
+				} catch (UnsupportedEncodingException e) {
+					e.printStackTrace();
+					return false;
+				}
+				player.sendMessage(prefix + ChatColor.BLUE + args[0]
+							+ ChatColor.YELLOW + " gespeichert.");
+				return true;
+			} else {
+				player.sendMessage(prefix + ChatColor.RED + "Du hast noch keine Punkte gesetzt.");
+				return true;
+			}
+		}
+		case "load":{
+			if(!player.hasPermission("camerastudio.load")){
+				return true;
+			}
+			if (CameraStudio.isTravelling(player.getUniqueId())) {
+				player.sendMessage(prefix + ChatColor.RED + "Du bewegst dich bereits auf einem Pfad.");
+				return true;
+			}
+			if(args.length<1) return false;
+			YamlConfiguration yamlConfiguration;
+			try {
+				yamlConfiguration = DataSource.getYamlResponse("camera_studio/load_path.php", new String[]{
+						"name="+URLEncoder.encode(args[0], "utf-8")	
+					});
+			} catch (UnsupportedEncodingException e) {
+				return true;
+			}
+			if(yamlConfiguration==null || !yamlConfiguration.contains("path")){
+				sender.sendMessage(prefix + ChatColor.RED + "Pfad '"+args[0]+"' nicht gefunden.");
+				return true;
+			}
+			ConfigurationSection pathSection = yamlConfiguration.getConfigurationSection("path");
+			CameraPath cameraPath = new CameraPath(player.getWorld(),pathSection);
+			List<Location> listOfLocations = new ArrayList<Location>();
+			listOfLocations.addAll(cameraPath.getPoints());
+			points.put(player.getUniqueId(), listOfLocations);
+			player.sendMessage(prefix + ChatColor.YELLOW + "Pfad: " + ChatColor.BLUE + cameraPath.getName() + ChatColor.YELLOW + " geladen!");
+			return true;
+		}
+		default: return false;
+		}
+	}
+}
---
+++ CamStudio/eu/crushedpixel/camerastudio/CameraStudio.java
@@ -0,0 +1,246 @@
+package eu.crushedpixel.camerastudio;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Chunk;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.craftbukkit.v1_12_R1.CraftChunk;
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.minecraft.server.v1_12_R1.PacketPlayOutMapChunk;
+import net.minecraft.server.v1_12_R1.PlayerConnection;
+
+public class CameraStudio extends JavaPlugin implements Listener {
+	public static JavaPlugin instance;
+	static String prefix = ChatColor.AQUA + "[" + ChatColor.DARK_AQUA + "CP" + ChatColor.AQUA + "CameraStudio] "
+			+ ChatColor.GREEN;
+	static HashSet<UUID> travelling = new HashSet<UUID>();
+	static HashSet<UUID> stopping = new HashSet<UUID>();
+
+	public void onDisable() {
+		getLogger().info("CameraStudio disabled");
+	}
+
+	public void onEnable() {
+
+		instance = this;
+
+		getServer().getPluginManager().registerEvents(this, this);
+		this.getCommand("cam").setExecutor(new CamCommand());
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+		getLogger().info(prefix + "CPCameraStudioReborn has been enabled!");
+	}
+
+	public static double round(double unrounded, int precision) {
+		BigDecimal bd = new BigDecimal(unrounded);
+		BigDecimal rounded = bd.setScale(precision, 4);
+		return rounded.doubleValue();
+	}
+
+	@EventHandler
+	public void onPlayerJoined(final PlayerJoinEvent event) {
+		if (getConfig().getBoolean("show-join-message")) {
+			getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+				public void run() {
+					event.getPlayer()
+							.sendMessage(prefix + "This server is running the Camera Studio Plugin v"
+									+ instance.getDescription().getVersion() + " by " + ChatColor.AQUA
+									+ "CrushedPixel. Updated by chrismin13.");
+					event.getPlayer().sendMessage(prefix + ChatColor.YELLOW + "http://youtube.com/CrushedPixel");
+				}
+			}, 10L);
+		}
+	}
+
+	@EventHandler
+	public void onPlayerLeave(final PlayerQuitEvent event) {
+		if (getConfig().getBoolean("clear-points-on-disconnect")
+				&& CamCommand.points.get(event.getPlayer().getUniqueId()) != null)
+			CamCommand.points.get(event.getPlayer().getUniqueId()).clear();
+	}
+	
+	protected static CameraPath loadPath(int path_id, World world){
+		return CameraPath.load(path_id, world);
+	}
+	
+	public static void travel(final Player player, int path_id, int time){
+		CameraPath path = loadPath(path_id, player.getWorld());
+		travel(player,path,time);
+	}
+	
+	public static void travel(final Player player, CameraPath path, int time){
+		travel(player,path.getPoints(),time*20,null,null);
+	}
+
+	public static void travel(final Player player, List<Location> locations, int time, String FailMessage,
+			final String CompletedMessage) {
+		List<Double> diffs = new ArrayList<Double>();
+		List<Integer> travelTimes = new ArrayList<Integer>();
+
+		double totalDiff = 0.0D;
+
+		for (int i = 0; i < locations.size() - 1; i++) {
+			Location s = (Location) locations.get(i);
+			Location n = (Location) locations.get(i + 1);
+			double diff = CameraStudio.positionDifference(s, n);
+			totalDiff += diff;
+			diffs.add(Double.valueOf(diff));
+		}
+
+		for (Iterator<Double> n = diffs.iterator(); n.hasNext();) {
+			double d = ((Double) n.next()).doubleValue();
+			travelTimes.add(Integer.valueOf((int) (d / totalDiff * time)));
+		}
+
+		final List<Location> tps = new ArrayList<Location>();
+
+		World w = player.getWorld();
+
+		for (int i = 0; i < locations.size() - 1; i++) {
+			Location s = (Location) locations.get(i);
+			Location n = (Location) locations.get(i + 1);
+			int t = ((Integer) travelTimes.get(i)).intValue();
+
+			double moveX = n.getX() - s.getX();
+			double moveY = n.getY() - s.getY();
+			double moveZ = n.getZ() - s.getZ();
+			double movePitch = n.getPitch() - s.getPitch();
+
+			double yawDiff = Math.abs(n.getYaw() - s.getYaw());
+			double c = 0.0D;
+
+			if (yawDiff <= 180.0D) {
+				if (s.getYaw() < n.getYaw()) {
+					c = yawDiff;
+				} else {
+					c = -yawDiff;
+				}
+			} else if (s.getYaw() < n.getYaw()) {
+				c = -(360.0D - yawDiff);
+			} else {
+				c = 360.0D - yawDiff;
+			}
+
+			double d = c / t;
+
+			for (int x = 0; x < t; x++) {
+				Location l = new Location(w, s.getX() + moveX / t * x, s.getY() + moveY / t * x,
+						s.getZ() + moveZ / t * x, (float) (s.getYaw() + d * x),
+						(float) (s.getPitch() + movePitch / t * x));
+				tps.add(l);
+			}
+
+		}
+
+		try {
+			final GameMode oldGameMode = player.getGameMode();
+			player.setGameMode(GameMode.SPECTATOR);
+			Location start = tps.get(0);
+			player.teleport(start);
+			loadChunkArea(player,start.getBlockX()>>4,start.getBlockZ()>>4,10);
+			travelling.add(player.getUniqueId());
+			Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(CameraStudio.instance, new Runnable() {
+				private int ticks = 0;
+
+				public void run() {
+					if (this.ticks < tps.size()) {
+
+						player.teleport((Location) tps.get(this.ticks));
+
+						if (!stopping.contains(player.getUniqueId())) {
+							Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(CameraStudio.instance, this, 1L);
+						} else {
+							stopping.remove(player.getUniqueId());
+							travelling.remove(player.getUniqueId());
+						}
+
+						this.ticks += 1;
+					} else {
+						travelling.remove(player.getUniqueId());
+						if (CompletedMessage != null)
+							player.sendMessage(CompletedMessage);
+						player.setGameMode(oldGameMode);
+					}
+				}
+			}, 1L);
+		} catch (Exception e) {
+			if (FailMessage != null)
+				player.sendMessage(FailMessage);
+		}
+	}
+	
+	public static void loadChunkArea(Player player, int chunk_x, int chunk_z, int radius){
+		World world = player.getWorld();
+		chunk_x-=radius;
+		chunk_z-=radius;
+		PlayerConnection playerConnection = ((CraftPlayer)player).getHandle().playerConnection;
+		Chunk chunk;
+		for(int x = 0; x < radius*2+1; x++){
+			for(int z = 0; z < radius*2+1; z++){
+				chunk = world.getChunkAt(chunk_x+x, chunk_z+z);
+				if(!chunk.isLoaded())chunk.load();
+				playerConnection.sendPacket(new PacketPlayOutMapChunk(((CraftChunk)chunk).getHandle(),65535));
+			}
+		}
+	}
+
+	public static double positionDifference(Location cLoc, Location eLoc) {
+		double cX = cLoc.getX();
+		double cY = cLoc.getY();
+		double cZ = cLoc.getZ();
+
+		double eX = eLoc.getX();
+		double eY = eLoc.getY();
+		double eZ = eLoc.getZ();
+
+		double dX = eX - cX;
+		if (dX < 0.0D) {
+			dX = -dX;
+		}
+		double dZ = eZ - cZ;
+		if (dZ < 0.0D) {
+			dZ = -dZ;
+		}
+		double dXZ = Math.hypot(dX, dZ);
+
+		double dY = eY - cY;
+		if (dY < 0.0D) {
+			dY = -dY;
+		}
+		double dXYZ = Math.hypot(dXZ, dY);
+
+		return dXYZ;
+	}
+
+	public static boolean isTravelling(UUID PlayerUUID) {
+		if (travelling.contains(PlayerUUID))
+			return true;
+		return false;
+	}
+
+	public static void stop(final UUID playerUUID) {
+		stopping.add(playerUUID);
+		Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(CameraStudio.instance, new Runnable() {
+			public void run() {
+				stopping.remove(playerUUID);
+			}
+		}, 2L);
+	}
+
+}
