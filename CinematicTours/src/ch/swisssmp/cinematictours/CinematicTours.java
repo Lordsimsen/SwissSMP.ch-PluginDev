447bf1bd1b10ca2af2fcde0ae7899323d42b1e33
BuildTools <star.etoa@hotmail.com>
2018-06-28 04:57:33 +0200

    CinematicTours entwickelt

---
+++ CinematicTours/src/ch/swisssmp/cinematictours/CinematicTours.java
@@ -0,0 +1,29 @@
+package ch.swisssmp.cinematictours;
+
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CinematicTours extends JavaPlugin{
+	private static Logger logger;
+	private static PluginDescriptionFile pdfFile;
+	
+	@Override
+	public void onEnable() {
+		pdfFile = getDescription();
+		logger = Logger.getLogger("Minecraft");
+		logger.info(pdfFile.getName() + " has been enabled (Version: " + pdfFile.getVersion() + ")");
+		
+		Bukkit.getPluginManager().registerEvents(new EventListener(), this);
+	}
+    
+	@Override
+	public void onDisable() {
+		HandlerList.unregisterAll(this);
+		PluginDescriptionFile pdfFile = getDescription();
+		logger.info(pdfFile.getName() + " has been disabled (Version: " + pdfFile.getVersion() + ")");
+	}
+}
---
+++ CinematicTours/src/ch/swisssmp/cinematictours/EventListener.java
@@ -0,0 +1,44 @@
+package ch.swisssmp.cinematictours;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Sign;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import ch.swisssmp.utils.SwissSMPler;
+
+public class EventListener implements Listener {
+	@EventHandler
+	private void onBlockPlace(SignChangeEvent event){
+		if(!event.getLine(0).equals("[Sightseeing]")) return;
+		if(!event.getPlayer().hasPermission("cinematictours.admin")){
+			event.getPlayer().sendMessage("["+ChatColor.DARK_GREEN+"Sightseeing"+ChatColor.RESET+"] Du kannst keine Besichtigungstouren einrichten.");
+			return;
+		}
+		event.setLine(0, ChatColor.DARK_GREEN+"[Sightseeing]");
+		event.setLine(2, ChatColor.DARK_GRAY+"Für Tour");
+		event.setLine(3, ChatColor.DARK_GRAY+"anklicken.");
+	}
+	@EventHandler
+	private void onBlockInteract(PlayerInteractEvent event){
+		if(event.getPlayer().getGameMode()==GameMode.SPECTATOR) return;
+		if(event.getAction()!=Action.RIGHT_CLICK_BLOCK) return;
+		if(event.getClickedBlock().getType()!=Material.WALL_SIGN && event.getClickedBlock().getType()!=Material.SIGN_POST) return;
+
+		BlockState state = event.getClickedBlock().getState();
+		if(!(state instanceof Sign))
+			return;
+		Sign sign = (Sign) state;
+		if(!sign.getLine(0).toLowerCase().contains(ChatColor.DARK_GREEN+"[sightseeing]"))
+			return;
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "cam sequence Sightseeing_"+sign.getLine(1)+" "+event.getPlayer().getName());
+		SwissSMPler.get(event.getPlayer()).sendTitle("", sign.getLine(1));
+	}
+}
