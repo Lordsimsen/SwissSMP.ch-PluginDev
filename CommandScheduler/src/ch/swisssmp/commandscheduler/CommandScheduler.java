7adad23802561c8379ea28ada0f34ded5f263f3b
BuildTools <star.etoa@hotmail.com>
2018-06-28 04:59:09 +0200

    CommandScheduler aktualisiert

--- CommandScheduler/src/ch/swisssmp/commandscheduler/CommandScheduler.java
+++ CommandScheduler/src/ch/swisssmp/commandscheduler/CommandScheduler.java
@@ -1,6 +1,5 @@
 package ch.swisssmp.commandscheduler;
 
-import java.util.List;
 import java.util.UUID;
 import java.util.logging.Logger;
 
@@ -9,13 +8,17 @@ import org.bukkit.command.CommandSender;
 import ch.swisssmp.utils.ConfigurationSection;
 import ch.swisssmp.utils.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 
 import ch.swisssmp.webcore.DataSource;
 
-public class CommandScheduler extends JavaPlugin{
+public class CommandScheduler extends JavaPlugin implements Listener{
 	private static Logger logger;
 	protected static PluginDescriptionFile pdfFile;
 	protected static CommandScheduler plugin;
@@ -33,14 +36,38 @@ public class CommandScheduler extends JavaPlugin{
 		PlayerCommand playerCommand = new PlayerCommand();
 		this.getCommand("commandscheduler").setExecutor(playerCommand);
 		refresh();
+		
+		Bukkit.getPluginManager().registerEvents(this, this);
 		logger.info(pdfFile.getName() + " has been enabled (Version: " + pdfFile.getVersion() + ")");
 	}
 
 	@Override
 	public void onDisable() {
+		HandlerList.unregisterAll((JavaPlugin)this);
 		if(this.refreshTask!=null) this.refreshTask.cancel();
 		PluginDescriptionFile pdfFile = getDescription();
 		logger.info(pdfFile.getName() + " has been disabled (Version: " + pdfFile.getVersion() + ")");
+	}
+	
+	@EventHandler
+	private void onPlayerJoin(PlayerJoinEvent event){
+		Bukkit.getScheduler().runTaskLater(this, new Runnable(){
+			public void run(){
+				runScheduledPlayerJoinCommands(event.getPlayer());
+			}
+		}, 2L);
+	}
+	
+	private void runScheduledPlayerJoinCommands(Player player){
+		if(!player.isOnline()) return;
+		YamlConfiguration yamlConfiguration = DataSource.getYamlResponse("commands/get_player_join.php", new String[]{
+				"player_uuid="+player.getUniqueId()
+		});
+		if(yamlConfiguration==null || !yamlConfiguration.contains("commands")) return;
+		for(String command : yamlConfiguration.getStringList("commands")){
+			Bukkit.getLogger().info("Performing command "+command);
+			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
+		}
 	}
 	
 	private void refresh(){
@@ -73,15 +100,6 @@ public class CommandScheduler extends JavaPlugin{
 			ConfigurationSection commandSection = yamlConfiguration.getConfigurationSection(key);
 			String commandLine = commandSection.getString("command");
 			String sender = commandSection.getString("sender");
-			List<String> argsList = commandSection.getStringList("args");
-			String[] args;
-			if(argsList==null){
-				args = null;
-			}
-			else{
-				args = new String[argsList.size()];
-				argsList.toArray(args);
-			}
 			CommandSender commandSender;
 			if(sender.equals("console")){
 				commandSender = Bukkit.getConsoleSender();
