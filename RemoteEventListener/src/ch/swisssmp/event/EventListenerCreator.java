9c07d3a36efcc3aed601e56c7c89c5271140db69
detig_iii <star.etoa@hotmail.com>
2018-07-31 15:23:27 +0200

    Veraltete Schnittstellen entfernt

--- RemoteEventListener/src/ch/swisssmp/event/EventListenerCreator.java
+++ RemoteEventListener/src/ch/swisssmp/event/EventListenerCreator.java
@@ -2,12 +2,8 @@ package ch.swisssmp.event;
 
 import ch.swisssmp.event.pluginlisteners.EventListenerMaster;
 import ch.swisssmp.event.remotelisteners.BasicEventListener;
-import ch.swisssmp.event.remotelisteners.CampClearEventListener;
-import ch.swisssmp.event.remotelisteners.CampClearedEventListener;
-import ch.swisssmp.event.remotelisteners.CampTriggerEventListener;
 import ch.swisssmp.event.remotelisteners.DungeonEventListener;
 import ch.swisssmp.event.remotelisteners.DungeonJoinEventListener;
-import ch.swisssmp.event.remotelisteners.ItemDiscoveredEventListener;
 import ch.swisssmp.event.remotelisteners.RegionEventListener;
 import ch.swisssmp.event.remotelisteners.TransformationEventListener;
 import ch.swisssmp.utils.ConfigurationSection;
@@ -16,13 +12,9 @@ public class EventListenerCreator {
 	public static BasicEventListener create(ConfigurationSection dataSection){
 		if(dataSection==null) return null;
 		switch(dataSection.getString("type")){
-		case "CampTriggerEvent": if(EventListenerMaster.getInst().getAdventureDungeonsLoaded()) return new CampTriggerEventListener(dataSection); break;
-		case "CampClearEvent": if(EventListenerMaster.getInst().getAdventureDungeonsLoaded()) return new CampClearEventListener(dataSection); break;
-		case "CampClearedEvent": if(EventListenerMaster.getInst().getAdventureDungeonsLoaded()) return new CampClearedEventListener(dataSection); break;
 		case "DungeonEndEvent": if(EventListenerMaster.getInst().getAdventureDungeonsLoaded()) return new DungeonEventListener(dataSection); break;
 		case "DungeonJoinEvent": if(EventListenerMaster.getInst().getAdventureDungeonsLoaded()) return new DungeonJoinEventListener(dataSection); break;
 		case "DungeonStartEvent": if(EventListenerMaster.getInst().getAdventureDungeonsLoaded()) return new DungeonEventListener(dataSection); break;
-		case "ItemDiscoveredEvent": if(EventListenerMaster.getInst().getAdventureDungeonsLoaded()) return new ItemDiscoveredEventListener(dataSection); break;
 		case "RegionEnterEvent": if(EventListenerMaster.getInst().getWGRegionEventsLoaded()) return new RegionEventListener(dataSection); break;
 		case "RegionEnteredEvent": if(EventListenerMaster.getInst().getWGRegionEventsLoaded()) return new RegionEventListener(dataSection); break;
 		case "RegionLeaveEvent": if(EventListenerMaster.getInst().getWGRegionEventsLoaded()) return new RegionEventListener(dataSection); break;
--- RemoteEventListener/src/ch/swisssmp/event/pluginlisteners/AdventureDungeonsListener.java
+++ RemoteEventListener/src/ch/swisssmp/event/pluginlisteners/AdventureDungeonsListener.java
@@ -3,22 +3,11 @@ package ch.swisssmp.event.pluginlisteners;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
-import ch.swisssmp.adventuredungeons.event.CampClearEvent;
-import ch.swisssmp.adventuredungeons.event.CampTriggerEvent;
 import ch.swisssmp.adventuredungeons.event.DungeonEndEvent;
 import ch.swisssmp.adventuredungeons.event.DungeonJoinEvent;
 import ch.swisssmp.adventuredungeons.event.DungeonStartEvent;
-import ch.swisssmp.adventuredungeons.event.ItemDiscoveredEvent;
 
 public class AdventureDungeonsListener implements Listener{
-	@EventHandler
-	private void CampTriggerEvent(CampTriggerEvent event){
-		EventListenerMaster.getInst().trigger(event);
-	}
-	@EventHandler
-	private void CampClearEvent(CampClearEvent event){
-		EventListenerMaster.getInst().trigger(event);
-	}
 	@EventHandler
 	private void DungeonEndEvent(DungeonEndEvent event){
 		EventListenerMaster.getInst().trigger(event);
@@ -29,10 +18,6 @@ public class AdventureDungeonsListener implements Listener{
 	}
 	@EventHandler
 	private void DungeonStartEvent(DungeonStartEvent event){
-		EventListenerMaster.getInst().trigger(event);
-	}
-	@EventHandler
-	private void ItemDiscoveredEvent(ItemDiscoveredEvent event){
 		EventListenerMaster.getInst().trigger(event);
 	}
 }
