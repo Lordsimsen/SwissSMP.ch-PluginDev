bc1282c82f367ba779c9ad40ae32bebbb01a371b
detig_iii <star.etoa@hotmail.com>
2018-08-25 12:38:15 +0200

    Laternen abbaubar gemacht

--- FlyingLanterns/src/ch/swisssmp/flyinglanterns/EventListener.java
+++ FlyingLanterns/src/ch/swisssmp/flyinglanterns/EventListener.java
@@ -1,22 +1,31 @@
 package ch.swisssmp.flyinglanterns;
 
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.event.world.ChunkUnloadEvent;
 import org.bukkit.inventory.ItemStack;
 
 import ch.swisssmp.customitems.CustomItems;
+import ch.swisssmp.utils.Mathf;
+import ch.swisssmp.utils.Random;
 
 public class EventListener implements Listener{
-	
+	private Random random = new Random();
 	@EventHandler
 	private void onLanternIgnite(PlayerInteractEvent event){
 		if(event.getAction()!=Action.RIGHT_CLICK_BLOCK) return;
@@ -24,15 +33,49 @@ public class EventListener implements Listener{
 		ItemStack itemStack = event.getItem();
 		String customEnum = CustomItems.getCustomEnum(itemStack);
 		if(customEnum==null || !customEnum.equals("FLYING_LANTERN")) return;
-		FlyingLantern.spawn(event.getClickedBlock().getRelative(event.getBlockFace()).getLocation().add(0.5,0.1,0.5));
+		Block block = event.getClickedBlock();
+		if(block.getType().isSolid()) block = block.getRelative(event.getBlockFace());
+		FlyingLantern.spawn(block.getLocation().add(0.5,0.1,0.5));
 		if(event.getPlayer().getGameMode()!=GameMode.CREATIVE) event.getItem().setAmount(event.getItem().getAmount()-1);
+		event.setCancelled(true);
 	}
 	
 	@EventHandler
-	private void onPlayerInteractEntity(PlayerInteractEntityEvent event){
-		FlyingLantern lantern = FlyingLantern.get(event.getRightClicked());
-		if(lantern!=null) event.setCancelled(true);
-		//TODO make breaking possible
+	private void onEntityDamageByEntity(EntityDamageByEntityEvent event){
+		if(event.getEntity().getType()!=EntityType.ARMOR_STAND) return;
+		FlyingLantern lantern = FlyingLantern.get(event.getEntity());
+		if(lantern==null) return;
+		if(event.getCause() == DamageCause.ENTITY_ATTACK){
+			if(event.getDamager() instanceof Player){
+				Player player = (Player) event.getDamager();
+				if(player.getGameMode()==GameMode.CREATIVE){
+					lantern.remove();
+					return;
+				}
+			}
+		}
+		else if(event.getDamager() instanceof Firework){
+			if(event.getDamager().getLocation().distanceSquared(event.getEntity().getLocation())>2) return;
+			Bukkit.getScheduler().runTaskLater(FlyingLanterns.plugin, new Runnable(){
+				public void run(){
+					lantern.explode();
+				}
+			}, 5+Mathf.floorToInt(random.nextDouble()*35));
+			lantern.explode();
+			return;
+		}
+		lantern.drop();
+	}
+	
+	@EventHandler
+	private void onProjectileHit(ProjectileHitEvent event){
+		Entity hit = event.getHitEntity();
+		if(hit==null) return;
+		FlyingLantern lantern = FlyingLantern.get(hit);
+		if(lantern!=null){
+			lantern.explode();
+			event.getEntity().remove();
+		}
 	}
 	
 	@EventHandler
--- FlyingLanterns/src/ch/swisssmp/flyinglanterns/FlyingLantern.java
+++ FlyingLanterns/src/ch/swisssmp/flyinglanterns/FlyingLantern.java
@@ -3,11 +3,18 @@ package ch.swisssmp.flyinglanterns;
 import java.util.HashMap;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
 import org.bukkit.Location;
 import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.FireworkEffect.Type;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.util.Vector;
 
@@ -32,20 +39,35 @@ public class FlyingLantern implements Runnable{
 	private FlyingLantern(Entity entity){
 		this.entity = entity;
 		this.maxSpeed = Mathf.clamp(random.nextDouble()*0.05, 0.01, 0.05);
-		this.rotation = (random.nextFloat()*2-1)*0.01f;
+		this.rotation = (random.nextFloat()*2-1)*0.3f;
 		this.motion = new Vector((random.nextDouble()*2-1)*0.3,1,(random.nextDouble()*2-1)*0.3);
 		lanternMap.put(entity, this);
+	}
+	
+	public Location getLocation(){
+		return this.entity.getLocation().add(0,0.75,0);
+	}
+	
+	public void drop(){
+		ItemStack itemStack = FlyingLanterns.getFlyingLantern();
+		this.entity.getWorld().dropItem(this.getLocation(), itemStack);
+		this.entity.getWorld().playSound(this.getLocation(), Sound.ENTITY_ITEMFRAME_REMOVE_ITEM, 10, 1);
+		this.remove();
 	}
 
 	@Override
 	public void run() {
 		this.speed = Mathf.clamp(this.speed+0.0001, 0, this.maxSpeed);
 		this.nextLocation = entity.getLocation().add(this.motion.clone().multiply(this.speed));
+		if(this.nextLocation.clone().add(0, 1, 0).getBlock().getType().isSolid()){
+			this.drop();
+			return;
+		}
 		this.nextLocation.setYaw(this.nextLocation.getYaw()+this.rotation);
 		this.entity.teleport(this.nextLocation);
 		this.flameCountdown--;
 		if(this.flameCountdown<0){
-			entity.getWorld().spawnParticle(Particle.FLAME, entity.getLocation().add(0,1.5,0), 1, 0.02,0.02,0.02, 0.003, null);
+			entity.getWorld().spawnParticle(Particle.FLAME, entity.getLocation().add(0,0.5,0), 1, 0.02,0.02,0.02, 0.003, null);
 			this.flameCountdown = 4;
 		}
 		if(entity.getLocation().getY()>300) this.remove();
@@ -56,7 +78,42 @@ public class FlyingLantern implements Runnable{
 		FlyingLantern.lanternMap.remove(this.entity);
 	}
 	
-	private void remove(){
+	protected void explode(){
+		Location location = this.getLocation();
+		Firework firework = (Firework)location.getWorld().spawnEntity(location, EntityType.FIREWORK);
+		FireworkMeta fireworkMeta = firework.getFireworkMeta();
+		
+        //Get the type
+        Type type = Type.BURST;
+       
+        //Create our effect with this
+        FireworkEffect effect = FireworkEffect.builder()
+        		.flicker(true)
+        		.withColor(Color.RED, Color.WHITE)
+        		.withFade(Color.YELLOW)
+        		.with(type)
+        		.trail(random.nextBoolean())
+        		.build();
+       
+        //Then apply the effect to the meta
+        fireworkMeta.addEffect(effect);
+       
+        //Generate some random power and set it
+        int rp = random.nextInt(2) + 1;
+        fireworkMeta.setPower(rp);
+       
+        //Then apply this to our rocket
+        firework.setFireworkMeta(fireworkMeta);
+        
+        Bukkit.getScheduler().runTaskLater(FlyingLanterns.plugin, new Runnable(){
+        	public void run(){
+        		firework.detonate();
+        	}
+        }, 1L);
+		this.remove();
+	}
+	
+	protected void remove(){
 		this.unload();
 		this.entity.remove();
 	}
@@ -64,11 +121,13 @@ public class FlyingLantern implements Runnable{
 	protected static FlyingLantern spawn(Location location){
 		location = location.clone();
 		location.setYaw(random.nextFloat()*360f);
-		ArmorStand armorStand = (ArmorStand) location.getWorld().spawnEntity(location.subtract(0, 1.5, 0), EntityType.ARMOR_STAND);
+		ArmorStand armorStand = (ArmorStand) location.getWorld().spawnEntity(location, EntityType.ARMOR_STAND);
 		armorStand.getEquipment().setHelmet(FlyingLanterns.getFlyingLantern());
 		armorStand.setVisible(false);
 		armorStand.setGravity(false);
 		armorStand.setInvulnerable(true);
+		armorStand.setSmall(true);
+		location.getWorld().playSound(location, Sound.BLOCK_CLOTH_PLACE, 5, 1);
 		return FlyingLantern.load(armorStand);
 	}
 	
