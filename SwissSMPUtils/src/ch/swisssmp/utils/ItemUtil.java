b466f711829cfba611ba340695761678bd6dee1f
detig_iii <star.etoa@hotmail.com>
2018-08-31 23:13:28 +0200

    ItemUtil erweitert

--- SwissSMPUtils/src/ch/swisssmp/utils/ItemUtil.java
+++ SwissSMPUtils/src/ch/swisssmp/utils/ItemUtil.java
@@ -1,7 +1,10 @@
 package ch.swisssmp.utils;
 
 import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_12_R1.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
+
+import net.minecraft.server.v1_12_R1.NBTTagCompound;
 
 public class ItemUtil {
 	public static boolean isHelmet(ItemStack itemStack){
@@ -46,5 +49,83 @@ public class ItemUtil {
 				material==Material.IRON_BOOTS||
 				material==Material.GOLD_BOOTS||
 				material==Material.DIAMOND_BOOTS;
+	}
+	
+	public static Position getPosition(ItemStack itemStack, String key){
+		net.minecraft.server.v1_12_R1.ItemStack nmsStack = CraftItemStack.asNMSCopy(itemStack);
+		if(!nmsStack.hasTag()) return null;
+		return NBTTagUtil.getPosition(nmsStack.getTag(), key);
+	}
+	
+	public static void setPosition(ItemStack itemStack, String key, Position position){
+		net.minecraft.server.v1_12_R1.ItemStack nmsStack = CraftItemStack.asNMSCopy(itemStack);
+		NBTTagCompound nbtTag = nmsStack.getTag();
+		NBTTagUtil.setPosition(nbtTag, key, position);
+		nmsStack.setTag(nbtTag);
+		itemStack.setItemMeta(CraftItemStack.getItemMeta(nmsStack));
+	}
+	
+	public static String getString(ItemStack itemStack, String key){
+		net.minecraft.server.v1_12_R1.ItemStack nmsStack = CraftItemStack.asNMSCopy(itemStack);
+		if(!nmsStack.hasTag()) return null;
+		NBTTagCompound nbtTag = nmsStack.getTag();
+		if(!nbtTag.hasKey(key)) return null;
+		return nbtTag.getString(key);
+	}
+	
+	public static void setString(ItemStack itemStack, String key, String value){
+		net.minecraft.server.v1_12_R1.ItemStack nmsStack = CraftItemStack.asNMSCopy(itemStack);
+		NBTTagCompound nbtTag = nmsStack.getTag();
+		nbtTag.setString(key, value);
+		nmsStack.setTag(nbtTag);
+		itemStack.setItemMeta(CraftItemStack.getItemMeta(nmsStack));
+	}
+	
+	public static int getInt(ItemStack itemStack, String key){
+		net.minecraft.server.v1_12_R1.ItemStack nmsStack = CraftItemStack.asNMSCopy(itemStack);
+		if(!nmsStack.hasTag()) return 0;
+		NBTTagCompound nbtTag = nmsStack.getTag();
+		if(!nbtTag.hasKey(key)) return 0;
+		return nbtTag.getInt(key);
+	}
+	
+	public static void setInt(ItemStack itemStack, String key, int value){
+		net.minecraft.server.v1_12_R1.ItemStack nmsStack = CraftItemStack.asNMSCopy(itemStack);
+		NBTTagCompound nbtTag = nmsStack.getTag();
+		nbtTag.setInt(key, value);
+		nmsStack.setTag(nbtTag);
+		itemStack.setItemMeta(CraftItemStack.getItemMeta(nmsStack));
+	}
+	
+	public static double getDouble(ItemStack itemStack, String key){
+		net.minecraft.server.v1_12_R1.ItemStack nmsStack = CraftItemStack.asNMSCopy(itemStack);
+		if(!nmsStack.hasTag()) return 0;
+		NBTTagCompound nbtTag = nmsStack.getTag();
+		if(!nbtTag.hasKey(key)) return 0;
+		return nbtTag.getDouble(key);
+	}
+	
+	public static void setDouble(ItemStack itemStack, String key, double value){
+		net.minecraft.server.v1_12_R1.ItemStack nmsStack = CraftItemStack.asNMSCopy(itemStack);
+		NBTTagCompound nbtTag = nmsStack.getTag();
+		nbtTag.setDouble(key, value);
+		nmsStack.setTag(nbtTag);
+		itemStack.setItemMeta(CraftItemStack.getItemMeta(nmsStack));
+	}
+	
+	public static boolean getBoolean(ItemStack itemStack, String key){
+		net.minecraft.server.v1_12_R1.ItemStack nmsStack = CraftItemStack.asNMSCopy(itemStack);
+		if(!nmsStack.hasTag()) return false;
+		NBTTagCompound nbtTag = nmsStack.getTag();
+		if(!nbtTag.hasKey(key)) return false;
+		return nbtTag.getBoolean(key);
+	}
+	
+	public static void setBoolean(ItemStack itemStack, String key, boolean value){
+		net.minecraft.server.v1_12_R1.ItemStack nmsStack = CraftItemStack.asNMSCopy(itemStack);
+		NBTTagCompound nbtTag = nmsStack.getTag();
+		nbtTag.setBoolean(key, value);
+		nmsStack.setTag(nbtTag);
+		itemStack.setItemMeta(CraftItemStack.getItemMeta(nmsStack));
 	}
 }
