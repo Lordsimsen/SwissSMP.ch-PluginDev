9b9bab4b76f3c79f34b787deab229c40bb954470
detig_iii <star.etoa@hotmail.com>
2020-03-01 22:56:12 +0100

    Update auf 1.15

--- StairChairs/src/ch/swisssmp/stairchairs/EventListener.java
+++ StairChairs/src/ch/swisssmp/stairchairs/EventListener.java
@@ -1,8 +1,11 @@
 package ch.swisssmp.stairchairs;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.Bisected.Half;
@@ -29,48 +32,81 @@ import org.bukkit.util.Vector;
 import org.spigotmc.event.entity.EntityDismountEvent;
 
 public class EventListener implements Listener {
+	
+	private static final String[] WoodTypes = {"oak","birch","spruce","jungle","dark_oak","acacia"};
+	
+	private static final boolean DebugOutput = false;
+	
 	@EventHandler
 	private void onPlayerInteract(PlayerInteractEvent event){
-		if(event.getHand()!=EquipmentSlot.HAND) return;
-		if(event.getAction()!=Action.RIGHT_CLICK_BLOCK) return;
-		if(event.getPlayer().getVehicle()!=null) return;
-		if(event.getItem()!=null) return;
+		if(event.getHand()!=EquipmentSlot.HAND) {
+			debug("Not Mainhand");
+			return;
+		}
+		if(event.getAction()!=Action.RIGHT_CLICK_BLOCK) {
+			debug("Not RightClickBlock");
+			return;
+		}
+		Player player = event.getPlayer();
+		if((player.getGameMode()!=GameMode.SURVIVAL && player.getGameMode()!=GameMode.CREATIVE) || player.getVehicle()!=null) {
+			debug("Wrong GameMode or riding a vehicle");
+			return;
+		}
+		if(event.getItem()!=null) {
+			debug("Item in Hand");
+			return;
+		}
 		Block block = event.getClickedBlock();
-		if(isOccupied(block.getRelative(BlockFace.UP))) return;
-		if(!isChair(block)) return;
-		if(StairChairs.playerMap.containsKey(block)) return;
+		if(isObstructed(block.getRelative(BlockFace.UP))) {
+			debug("Obstructed");
+			return;
+		}
+		if(!isChair(block)) {
+			debug("Not a chair");
+			return;
+		}
+		if(StairChairs.playerMap.containsKey(block)) {
+			debug("Occupied");
+			return;
+		}
 		Location location = block.getLocation();
 		location.add(0.5, 0, 0.5);
-		if(event.getPlayer().getLocation().distanceSquared(location)>1.75){
+		if(player.getLocation().distanceSquared(location)>1.75){
+			debug("Out of range");
 			return;
 		}
 		location.add(0, -1.15, 0);
-		if(block.getState().getData() instanceof Stairs){
-			Stairs stairs = (Stairs)block.getState().getData();
-			if(stairs.getHalf()==Half.TOP) return;
+		BlockData data = block.getBlockData();
+		if(data instanceof Stairs){
+			Stairs stairs = (Stairs) data;
+			if(stairs.getHalf()==Half.TOP) {
+				debug("Stairs upside down");
+				return;
+			}
 			double offset = 0.2;
 			switch(stairs.getFacing()){
-			case NORTH:
+			case SOUTH:
 				location = location.add(0, 0, -offset);
 				location.setDirection(new Vector(0,0,-1));
 				break;
-			case EAST:
+			case WEST:
 				location = location.add(offset, 0, 0);
 				location.setDirection(new Vector(1,0,0));
 				break;
-			case SOUTH:
+			case NORTH:
 				location = location.add(0, 0, offset);
 				location.setDirection(new Vector(0,0,1));
 				break;
-			case WEST:
+			case EAST:
 				location = location.add(-offset, 0, 0);
 				location.setDirection(new Vector(-1,0,0));
 				break;
 			default:
+				debug("What is this direction: "+stairs.getFacing());
 				return;
 			}
 		}
-		else if(block.getState().getData() instanceof Slab){
+		else if(data instanceof Slab){
 			Vector direction = new Vector(0, 0, 0);
 			Block north = block.getRelative(BlockFace.NORTH);
 			Block east = block.getRelative(BlockFace.EAST);
@@ -104,17 +140,32 @@ public class EventListener implements Listener {
 				location.setDirection(direction);
 			}
 		}
-		else return;
-		StairChairs.locationMap.put(event.getPlayer(), event.getPlayer().getLocation().clone());
-		ArmorStand armorStand = (ArmorStand)location.getWorld().spawnEntity(location, EntityType.ARMOR_STAND);
+		else {
+			debug("Neither stair nor slab");
+			return;
+		}
+		StairChairs.locationMap.put(player, player.getLocation().clone());
+		final Location chairLocation = location.clone();
+		final Location soundLocation = location.clone().add(0, 1, 0);
+		ArmorStand armorStand = (ArmorStand) location.getWorld().spawnEntity(chairLocation, EntityType.ARMOR_STAND);
 		armorStand.setInvulnerable(true);
 		armorStand.setVisible(false);
 		armorStand.setGravity(false);
-		armorStand.addPassenger(event.getPlayer());
 		armorStand.setCustomName("§cStairChair");
 		armorStand.setCustomNameVisible(false);
-		StairChairs.playerMap.put(block, event.getPlayer());
+		armorStand.addPassenger(player);
+		soundLocation.getWorld().playSound(soundLocation, isWood(block) ? Sound.BLOCK_WOOD_STEP : Sound.BLOCK_STONE_STEP, SoundCategory.BLOCKS, 1, 2f);
+		StairChairs.playerMap.put(block, player);
 		StairChairs.entityMap.put(armorStand, block);
+		debug(player.getName()+" sits!");
+	}
+	
+	private static boolean isWood(Block block) {
+		String typeString = block.getType().toString().toLowerCase();
+		for(String woodType : WoodTypes) {
+			if(typeString.contains(woodType)) return true;
+		}
+		return false;
 	}
 	
 	private static boolean isChair(Block block){
@@ -128,10 +179,15 @@ public class EventListener implements Listener {
 		return false;
 	}
 	
-	private static boolean isOccupied(Block block){
+	private static boolean isObstructed(Block block){
 		Material material = block.getType();
 		return  !(material == Material.AIR ||
-				material == Material.WALL_SIGN ||
+				material == Material.ACACIA_WALL_SIGN ||
+				material == Material.BIRCH_WALL_SIGN ||
+				material == Material.DARK_OAK_WALL_SIGN ||
+				material == Material.JUNGLE_WALL_SIGN ||
+				material == Material.OAK_WALL_SIGN ||
+				material == Material.SPRUCE_WALL_SIGN ||
 				material == Material.ITEM_FRAME ||
 				material == Material.PAINTING ||
 				material == Material.TORCH ||
@@ -234,6 +290,9 @@ public class EventListener implements Listener {
 		StairChairs.entityMap.remove(armorStand);
 		armorStand.eject();
 		armorStand.remove();
-		
+	}
+	
+	private static void debug(String s) {
+		if(DebugOutput) Bukkit.getLogger().info("["+StairChairs.pdfFile.getName()+"] "+s);
 	}
 }
