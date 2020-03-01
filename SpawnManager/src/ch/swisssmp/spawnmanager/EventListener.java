ea3cc00f0884d1e0a5616e2e6a2375049279afc9
detig_iii <star.etoa@hotmail.com>
2020-03-01 23:04:04 +0100

    EventListener separiert

---
+++ SpawnManager/src/ch/swisssmp/spawnmanager/EventListener.java
@@ -0,0 +1,135 @@
+package ch.swisssmp.spawnmanager;
+
+import java.util.Random;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.Statistic;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.type.Bed;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.potion.PotionEffect;
+
+import ch.swisssmp.utils.SwissSMPler;
+import ch.swisssmp.utils.URLEncoder;
+import ch.swisssmp.utils.YamlConfiguration;
+import ch.swisssmp.webcore.DataSource;
+import ch.swisssmp.webcore.HTTPRequest;
+import ch.swisssmp.webcore.RequestMethod;
+
+public class EventListener implements Listener {
+	
+	private Random random = new Random();
+	
+	@EventHandler
+	private void onPlayerJoin(PlayerJoinEvent event){
+		HTTPRequest request = DataSource.getResponse(SpawnManager.getInstance(), "last_world.php", new String[]{
+			"player="+event.getPlayer().getUniqueId()	
+		});
+		request.onFinish(()->{
+			String worldName = request.getResponse();
+			if(worldName==null || worldName.isEmpty()) return;
+			World world = Bukkit.getWorld(worldName);
+			if(world==null){
+				event.getPlayer().teleport(Bukkit.getWorlds().get(0).getSpawnLocation());
+				DataSource.getResponse(SpawnManager.getInstance(), "world_change.php", new String[]{
+						"player="+event.getPlayer().getUniqueId(),
+						"world="+event.getPlayer().getWorld().getName()
+				});
+			}
+		});
+	}
+	
+	@EventHandler(priority=EventPriority.HIGHEST,ignoreCancelled=true)
+	private void onPlayerInteractBlock(PlayerInteractEvent event){
+		if(event.getPlayer().getGameMode()!=GameMode.SURVIVAL || event.getAction()!=Action.RIGHT_CLICK_BLOCK) return;
+		Block block = event.getClickedBlock();
+		if(!(block.getBlockData() instanceof Bed)) return;
+		World world = block.getWorld();
+		long time = world.getTime();
+		boolean isDay = time < 12541 && !world.hasStorm();
+		Location previousBed = event.getPlayer().getBedSpawnLocation();
+		event.getPlayer().setBedSpawnLocation(event.getPlayer().getLocation(), true);
+		if(!isDay) return;
+		
+		event.setCancelled(true);
+		Bukkit.getScheduler().runTaskLater(SpawnManager.getInstance(), ()->{
+			event.getPlayer().setStatistic(Statistic.TIME_SINCE_REST, 0);
+		},10L);
+		
+		double distanceToPreviousBed = previousBed.distanceSquared(block.getLocation());
+		if(distanceToPreviousBed>36){
+			SwissSMPler.get(event.getPlayer()).sendActionBar(ChatColor.GREEN+"Spawpunkt gesetzt!");
+		}
+		else if(isDay){
+			SwissSMPler.get(event.getPlayer()).sendActionBar(ChatColor.YELLOW+"Du fühlst dich erholt.");
+		}
+	}
+	
+	@EventHandler
+	private void onPlayerChangedWorld(PlayerChangedWorldEvent event){
+		DataSource.getResponse(SpawnManager.getInstance(), "world_change.php", new String[]{
+			"player="+event.getPlayer().getUniqueId(),
+			"world="+event.getPlayer().getWorld().getName()
+		});
+	}
+	
+	@EventHandler(ignoreCancelled=true)
+	private void onPlayerRespawn(PlayerRespawnEvent event){
+		Player player = event.getPlayer();
+		if(event.isBedSpawn()){
+			return;
+		}
+		HTTPRequest request = DataSource.getResponse(SpawnManager.getInstance(), "spawn.php", new String[]{
+				"player="+player.getUniqueId().toString(),
+				"x="+(int)Math.round(player.getLocation().getX()),
+				"y="+(int)Math.round(player.getLocation().getY()),
+				"z="+(int)Math.round(player.getLocation().getZ()),
+				"world="+URLEncoder.encode(player.getWorld().getName()),
+				"flag=respawn"
+		}, RequestMethod.POST_SYNC);
+		YamlConfiguration yamlConfiguration = request.getYamlResponse();
+		if(yamlConfiguration==null) return;
+		if(yamlConfiguration.contains("spawnpoint")){
+			Location location = yamlConfiguration.getLocation("spawnpoint");
+			if(location!=null){
+				event.setRespawnLocation(location);
+			}
+		}
+		if(yamlConfiguration.contains("message")){
+			player.sendMessage(yamlConfiguration.getString("message"));
+		}
+		if(yamlConfiguration.contains("actionbar") || yamlConfiguration.contains("effect") || yamlConfiguration.contains("sound")){
+			Bukkit.getScheduler().runTaskLater(SpawnManager.plugin, new Runnable(){
+				public void run(){
+					if(yamlConfiguration.contains("actionbar")){
+						SwissSMPler swisssmpler = SwissSMPler.get(player);
+						swisssmpler.sendActionBar(yamlConfiguration.getString("actionbar"));
+					}
+					if(yamlConfiguration.contains("effect")){
+						PotionEffect potionEffect = yamlConfiguration.getPotionEffect("effect");
+						if(potionEffect!=null) player.addPotionEffect(potionEffect);
+					}
+					if(yamlConfiguration.contains("sound")){
+						Sound sound = Sound.valueOf(yamlConfiguration.getString("sound"));
+						if(sound!=null){
+							player.getWorld().playSound(player.getLocation(), sound, 5f, 0.8f+random.nextFloat()*0.4f);
+						}
+					}
+				}
+			}, 1l);
+		}
+	}
+}
--- SpawnManager/src/ch/swisssmp/spawnmanager/SpawnManager.java
+++ SpawnManager/src/ch/swisssmp/spawnmanager/SpawnManager.java
@@ -1,46 +1,19 @@
 package ch.swisssmp.spawnmanager;
 
 import java.io.File;
-import java.util.Random;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
-import org.bukkit.Location;
-import org.bukkit.Sound;
-import org.bukkit.Statistic;
-import org.bukkit.World;
-import org.bukkit.block.data.type.Bed;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerBedEnterEvent;
-import org.bukkit.event.player.PlayerChangedWorldEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.potion.PotionEffect;
 
-import ch.swisssmp.utils.SwissSMPler;
-import ch.swisssmp.utils.URLEncoder;
-import ch.swisssmp.utils.YamlConfiguration;
-import ch.swisssmp.webcore.DataSource;
-import ch.swisssmp.webcore.HTTPRequest;
-import ch.swisssmp.webcore.RequestMethod;
-
-public class SpawnManager extends JavaPlugin implements Listener{
+public class SpawnManager extends JavaPlugin {
 	protected static Logger logger;
 	protected static PluginDescriptionFile pdfFile;
 	protected static File dataFolder;
 	protected static SpawnManager plugin;
 	protected static boolean debug;
-	private static Random random = new Random();
 	
 	@Override
 	public void onEnable() {
@@ -49,7 +22,7 @@ public class SpawnManager extends JavaPlugin implements Listener{
 		logger = Logger.getLogger("Minecraft");
 		logger.info(pdfFile.getName() + " has been enabled (Version: " + pdfFile.getVersion() + ")");
 		
-		Bukkit.getPluginManager().registerEvents(this, this);
+		Bukkit.getPluginManager().registerEvents(new EventListener(), this);
 		PlayerCommand playerCommand = new PlayerCommand();
 		this.getCommand("spawn").setExecutor(playerCommand);
 		this.getCommand("settemplespawn").setExecutor(playerCommand);
@@ -57,98 +30,9 @@ public class SpawnManager extends JavaPlugin implements Listener{
 
 	@Override
 	public void onDisable() {
-		HandlerList.unregisterAll((JavaPlugin)this);
+		HandlerList.unregisterAll(this);
 		PluginDescriptionFile pdfFile = getDescription();
 		logger.info(pdfFile.getName() + " has been disabled (Version: " + pdfFile.getVersion() + ")");
-	}
-	
-	@EventHandler
-	private void onPlayerJoin(PlayerJoinEvent event){
-		HTTPRequest request = DataSource.getResponse(plugin, "last_world.php", new String[]{
-			"player="+event.getPlayer().getUniqueId()	
-		});
-		request.onFinish(()->{
-			String worldName = request.getResponse();
-			if(worldName==null || worldName.isEmpty()) return;
-			World world = Bukkit.getWorld(worldName);
-			if(world==null){
-				event.getPlayer().teleport(Bukkit.getWorlds().get(0).getSpawnLocation());
-				DataSource.getResponse(plugin, "world_change.php", new String[]{
-						"player="+event.getPlayer().getUniqueId(),
-						"world="+event.getPlayer().getWorld().getName()
-				});
-			}
-		});
-	}
-	
-	@EventHandler(priority=EventPriority.HIGHEST,ignoreCancelled=true)
-	private void onPlayerInteractBlock(PlayerInteractEvent event){
-		if(event.getPlayer().getGameMode()!=GameMode.SURVIVAL || event.getAction()!=Action.RIGHT_CLICK_BLOCK) return;
-		if(!(event.getClickedBlock().getBlockData() instanceof Bed)) return;
-		World world = event.getClickedBlock().getWorld();
-		event.getPlayer().setBedSpawnLocation(event.getPlayer().getLocation(), true);
-		event.setCancelled(world.getTime()<14000);
-		SwissSMPler.get(event.getPlayer()).sendActionBar(ChatColor.GREEN+"Spawpunkt gesetzt!");
-	}
-	
-	@EventHandler
-	private void onPlayerBedEnter(PlayerBedEnterEvent event){
-		event.getPlayer().setStatistic(Statistic.TIME_SINCE_REST, 0);
-	}
-	
-	@EventHandler
-	private void onPlayerChangedWorld(PlayerChangedWorldEvent event){
-		DataSource.getResponse(plugin, "world_change.php", new String[]{
-			"player="+event.getPlayer().getUniqueId(),
-			"world="+event.getPlayer().getWorld().getName()
-		});
-	}
-	
-	@EventHandler(ignoreCancelled=true)
-	private void onPlayerRespawn(PlayerRespawnEvent event){
-		Player player = event.getPlayer();
-		if(event.isBedSpawn()){
-			return;
-		}
-		HTTPRequest request = DataSource.getResponse(plugin, "spawn.php", new String[]{
-				"player="+player.getUniqueId().toString(),
-				"x="+(int)Math.round(player.getLocation().getX()),
-				"y="+(int)Math.round(player.getLocation().getY()),
-				"z="+(int)Math.round(player.getLocation().getZ()),
-				"world="+URLEncoder.encode(player.getWorld().getName()),
-				"flag=respawn"
-		}, RequestMethod.POST_SYNC);
-		YamlConfiguration yamlConfiguration = request.getYamlResponse();
-		if(yamlConfiguration==null) return;
-		if(yamlConfiguration.contains("spawnpoint")){
-			Location location = yamlConfiguration.getLocation("spawnpoint");
-			if(location!=null){
-				event.setRespawnLocation(location);
-			}
-		}
-		if(yamlConfiguration.contains("message")){
-			player.sendMessage(yamlConfiguration.getString("message"));
-		}
-		if(yamlConfiguration.contains("actionbar") || yamlConfiguration.contains("effect") || yamlConfiguration.contains("sound")){
-			Bukkit.getScheduler().runTaskLater(SpawnManager.plugin, new Runnable(){
-				public void run(){
-					if(yamlConfiguration.contains("actionbar")){
-						SwissSMPler swisssmpler = SwissSMPler.get(player);
-						swisssmpler.sendActionBar(yamlConfiguration.getString("actionbar"));
-					}
-					if(yamlConfiguration.contains("effect")){
-						PotionEffect potionEffect = yamlConfiguration.getPotionEffect("effect");
-						if(potionEffect!=null) player.addPotionEffect(potionEffect);
-					}
-					if(yamlConfiguration.contains("sound")){
-						Sound sound = Sound.valueOf(yamlConfiguration.getString("sound"));
-						if(sound!=null){
-							player.getWorld().playSound(player.getLocation(), sound, 5f, 0.8f+random.nextFloat()*0.4f);
-						}
-					}
-				}
-			}, 1l);
-		}
 	}
 	
 	public static SpawnManager getInstance(){
