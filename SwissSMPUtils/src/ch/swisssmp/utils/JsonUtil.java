53ebb4e6ceba4e3d79bbee6c092861d6b546171f
detig-iii <star.etoa@hotmail.com>
2020-04-23 19:40:26 +0200

    JsonUtil erweitert

--- SwissSMPUtils/src/ch/swisssmp/utils/JsonUtil.java
+++ SwissSMPUtils/src/ch/swisssmp/utils/JsonUtil.java
@@ -10,13 +10,40 @@ import org.bukkit.block.Block;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.UUID;
 
 public class JsonUtil {
+
+    public static boolean save(File file, JsonObject json){
+        try{
+            if(!file.getParentFile().isDirectory()){
+                if(!file.getParentFile().mkdirs()){
+                    return false;
+                }
+            }
+        }
+        catch(Exception e){
+            return false;
+        }
+        try {
+            FileWriter writer = new FileWriter(file);
+            writer.write(json.toString());
+            writer.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
     public static JsonObject parse(File file){
         JsonParser parser = new JsonParser();
         try {
             FileReader reader = new FileReader(file);
             JsonElement result = parser.parse(reader);
+            reader.close();
             return result.isJsonObject() ? result.getAsJsonObject() : null;
         } catch (Exception e) {
             return null;
@@ -34,9 +61,28 @@ public class JsonUtil {
         }
     }
 
+    public static UUID getUUID(String key, JsonObject json){
+        JsonElement element = json.has(key) ? json.get(key) : null;
+        String idString = element!=null && element.isJsonPrimitive() ? element.getAsString() : null;
+        try{
+            return idString!=null ? UUID.fromString(idString) : null;
+        }
+        catch(Exception e){
+            return null;
+        }
+    }
+
+    public static void set(String key, UUID uuid, JsonObject json){
+        json.addProperty(key, uuid.toString());
+    }
+
     public static String getString(String key, JsonObject json){
         JsonElement element = json.has(key) ? json.get(key) : null;
         return element!=null && element.isJsonPrimitive() ? element.getAsString() : null;
+    }
+
+    public static void set(String key, String value, JsonObject json){
+        json.addProperty(key, value);
     }
 
     public static float getFloat(String key, JsonObject json){
@@ -44,9 +90,17 @@ public class JsonUtil {
         return element!=null && element.isJsonPrimitive() ? element.getAsFloat() : 0;
     }
 
+    public static void set(String key, float value, JsonObject json){
+        json.addProperty(key, value);
+    }
+
     public static double getDouble(String key, JsonObject json){
         JsonElement element = json.has(key) ? json.get(key) : null;
         return element!=null && element.isJsonPrimitive() ? element.getAsDouble() : 0;
+    }
+
+    public static void set(String key, double value, JsonObject json){
+        json.addProperty(key, value);
     }
 
     public static int getInt(String key, JsonObject json){
@@ -54,13 +108,25 @@ public class JsonUtil {
         return element!=null && element.isJsonPrimitive() ? element.getAsInt() : 0;
     }
 
+    public static void set(String key, int value, JsonObject json){
+        json.addProperty(key, value);
+    }
+
     public static boolean getBool(String key, JsonObject json){
         JsonElement element = json.has(key) ? json.get(key) : null;
         return element!=null && element.isJsonPrimitive() && element.getAsBoolean();
     }
 
+    public static void set(String key, boolean value, JsonObject json){
+        json.addProperty(key, value);
+    }
+
     public static Color getColor(String key, JsonObject json){
         return Color.fromRGB(getInt(key,json));
+    }
+
+    public static void set(String key, Color value, JsonObject json){
+        json.addProperty(key, value.asRGB());
     }
 
     public static Block getBlock(String key, World world, JsonObject json){
@@ -71,6 +137,15 @@ public class JsonUtil {
         int y = getInt("y", locationData);
         int z = getInt("z", locationData);
         return world.getBlockAt(x,y,z);
+    }
+
+    public static void set(String key, Block block, JsonObject json){
+        JsonObject locationData = new JsonObject();
+        locationData.addProperty("x", block.getX());
+        locationData.addProperty("y", block.getY());
+        locationData.addProperty("z", block.getZ());
+
+        json.add(key, locationData);
     }
 
     public static Location getLocation(String key, World world, JsonObject json){
@@ -87,5 +162,16 @@ public class JsonUtil {
         }
 
         return new Location(world, x, y, z);
+    }
+
+    public static void set(String key, Location location, JsonObject json){
+        JsonObject locationData = new JsonObject();
+        locationData.addProperty("x", location.getX());
+        locationData.addProperty("y", location.getY());
+        locationData.addProperty("z", location.getZ());
+        locationData.addProperty("pitch", location.getPitch());
+        locationData.addProperty("yaw", location.getYaw());
+
+        json.add(key, locationData);
     }
 }
