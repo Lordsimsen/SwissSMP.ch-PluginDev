ac4f84afbcef2f529730d3583d077ae2f49f6be6
detig_iii <star.etoa@hotmail.com>
2020-06-15 08:36:36 +0200

    Auswahl des Ausschnitts verbessert

--- LivemapRenderAPI/src/ch/swisssmp/livemap_render_api/LivemapView.java
+++ LivemapRenderAPI/src/ch/swisssmp/livemap_render_api/LivemapView.java
@@ -1,5 +1,7 @@
 package ch.swisssmp.livemap_render_api;
 
+import ch.swisssmp.utils.Mathf;
+import org.bukkit.Bukkit;
 import org.bukkit.util.BlockVector;
 
 public class LivemapView {
@@ -25,37 +27,33 @@ public class LivemapView {
 	public String[][] getUrls(BlockVector min, BlockVector max){
 		
 		IsoBounds isoBounds = IsoBounds.calculate(min, max, livemapInfo.isoDirection, livemapInfo.isoAngle, livemapInfo.chunkSize);
-		int sizeX = (isoBounds.toX - isoBounds.fromX);
-		int sizeY = (isoBounds.toY - isoBounds.fromY);
+
+		double isoCenterX = isoBounds.fromX+(isoBounds.toX-isoBounds.fromX)/2f;
+		double isoCenterY = isoBounds.fromY+(isoBounds.toY-isoBounds.fromY)/2f;
+
+		int isoSizeX = (isoBounds.toX - isoBounds.fromX);
+		int isoSizeY = (isoBounds.toY - isoBounds.fromY);
 		
-		int zoomX = (int) Math.ceil(Math.log(sizeX/(double)mapSizeX) / Math.log(2) + 1e-10);
-		int zoomY = (int) Math.ceil(Math.log(sizeY/(double)mapSizeY) / Math.log(2) + 1e-10);
+		int zoomX = (int) Math.floor(Math.log(isoSizeX/(double)mapSizeX) / Math.log(2) + 1e-10);
+		int zoomY = (int) Math.floor(Math.log(isoSizeY/(double)mapSizeY) / Math.log(2) + 1e-10);
 		
 		int zoom = Math.min(livemapInfo.maxZoomLevel, Math.max(zoomX, zoomY));
 		int tileSize = Math.max(1, (int)(Math.pow(2, zoom)));
-		//int calculatedWidth = tileSize * mapSizeX;
-		//int calculatedHeight = tileSize * mapSizeY;
-		
-		double aspectRatio = mapSizeY / (double) mapSizeX;
 
-		double isoHalfWidthDiff = sizeX*aspectRatio<sizeY ? (sizeY/aspectRatio - sizeX) / 2.0 : 0;
-		double isoHalfHeightDiff = sizeY<sizeX*aspectRatio ? (sizeX*aspectRatio - sizeY) / 2.0 : 0;
-		
-		double leftIsoX = isoBounds.fromX - isoHalfWidthDiff;
-		double lowerIsoY = isoBounds.fromY - isoHalfHeightDiff;
-		
-		int leftX = (int)Math.floor(leftIsoX / tileSize) * tileSize;
-		int lowerY = (int)(Math.floor(lowerIsoY / tileSize)+1) * tileSize;
-		
-		/*
+		int centerTileX = Mathf.floorToInt(isoCenterX/tileSize+(mapSizeX%2!=0?-0.5f:0));
+		int centerTileY = Mathf.floorToInt(isoCenterY/tileSize+(mapSizeY%2!=0?-0.5f:0));
+
+		int leftX = (centerTileX-Mathf.floorToInt((mapSizeX)/2f))*tileSize;
+		int lowerY = (centerTileY-Mathf.floorToInt((mapSizeY)/2f)+1)*tileSize;
+
 		Bukkit.getLogger().info(
 				"Bounds: "+min.getBlockX()+","+min.getBlockY()+","+min.getBlockZ()+" to "+max.getBlockX()+","+max.getBlockY()+","+max.getBlockZ()+"\n"
 				+ "IsoBounds: "+isoBounds.fromX+","+isoBounds.fromY+" to "+isoBounds.toX+","+isoBounds.toY+"\n"
-				+ "Size: "+sizeX+","+sizeY+"\n"
+				+ "Size: "+isoSizeX+","+isoSizeY+"\n"
 				+ "Zoom: "+zoom+"\n"
 				+ "TileSize: "+tileSize+"\n"
 				+ "Calculated: "+leftX+","+lowerY+" to "+(leftX+(mapSizeX*tileSize))+","+(lowerY+(mapSizeY*tileSize)));
-				*/
+
 		
 		String zoomPrefix = getZoomPrefix(zoom);
 		String[][] result = new String[mapSizeY][mapSizeX];
@@ -63,19 +61,20 @@ public class LivemapView {
 			for(int x = 0; x < mapSizeX; x++) {
 				int tileX = leftX + x * tileSize;
 				int tileY = lowerY + y * tileSize;
-				result[y][x] = livemapInfo.url+zoomPrefix+tileX+"_"+tileY+".jpg";
+				String url = livemapInfo.url+zoomPrefix+tileX+"_"+tileY+".jpg";
+				result[y][x] = url;
 			}
 		}
 		return result;
 	}
 	
 	private String getZoomPrefix(int zoom) {
-		String result = "";
-		if(zoom==0) return result;
+		StringBuilder result = new StringBuilder();
+		if(zoom==0) return result.toString();
 		for(int i = 0; i < zoom; i++) {
-			result+="z";
+			result.append("z");
 		}
-		result+="_";
-		return result;
+		result.append("_");
+		return result.toString();
 	}
 }
