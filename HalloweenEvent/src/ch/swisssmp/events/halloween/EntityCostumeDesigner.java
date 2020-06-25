fc3beb2f15a6c3dd6863d1d5ab1023ebd523e93b
detig_iii <star.etoa@hotmail.com>
2020-06-25 14:52:28 +0200

    Update zu 1.16

--- HalloweenEvent/src/ch/swisssmp/events/halloween/EntityCostumeDesigner.java
+++ HalloweenEvent/src/ch/swisssmp/events/halloween/EntityCostumeDesigner.java
@@ -9,20 +9,23 @@ import org.bukkit.inventory.ItemStack;
 import ch.swisssmp.utils.Random;
 
 public class EntityCostumeDesigner {
-	private static Random random = new Random();
-	
-	protected static void applyCostume(Entity entity){
-		switch(entity.getType()){
-		case ZOMBIE:
-		case PIG_ZOMBIE:
-		case SKELETON:
-			if(random.nextDouble()>0.3) EntityCostumeDesigner.applyPumpkin((LivingEntity)entity); break;
-		default:return;
-		}
-	}
-	protected static void applyPumpkin(LivingEntity entity){
-		EntityEquipment equipment = entity.getEquipment();
-		if(equipment.getHelmet()!=null && equipment.getHelmet().getType()!=Material.AIR) return;
-		equipment.setHelmet(new ItemStack(Material.PUMPKIN));
-	}
+    private static Random random = new Random();
+
+    protected static void applyCostume(Entity entity) {
+        switch (entity.getType()) {
+            case ZOMBIE:
+            case ZOMBIFIED_PIGLIN:
+            case SKELETON:
+                if (random.nextDouble() > 0.3) EntityCostumeDesigner.applyPumpkin((LivingEntity) entity);
+                break;
+            default:
+                return;
+        }
+    }
+
+    protected static void applyPumpkin(LivingEntity entity) {
+        EntityEquipment equipment = entity.getEquipment();
+        if (equipment.getHelmet() != null && equipment.getHelmet().getType() != Material.AIR) return;
+        equipment.setHelmet(new ItemStack(Material.PUMPKIN));
+    }
 }
