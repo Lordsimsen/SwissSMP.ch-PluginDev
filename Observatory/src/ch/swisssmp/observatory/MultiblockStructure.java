f31b7da985fd52b264bc474998e0fbb88c1c9b31
WhiteGuggi <64141727+WhiteGuggi@users.noreply.github.com>
2020-07-02 20:49:38 +0200

    Observatorium weiterwntickelt

---
+++ Observatory/src/ch/swisssmp/observatory/MultiblockStructure.java
@@ -0,0 +1,13 @@
+package ch.swisssmp.observatory;
+
+import org.bukkit.block.Block;
+
+public class MultiblockStructure {
+
+    /**
+     * Should find out whether given block is part of the multiblockstructure.
+     */
+    public static boolean isMultiblockStructure(Block block){
+        return true;
+    }
+}
---
+++ Observatory/src/ch/swisssmp/observatory/Observatory.java
@@ -0,0 +1,37 @@
+package ch.swisssmp.observatory;
+
+import ch.swisssmp.city.City;
+import org.bukkit.Location;
+import org.bukkit.World;
+
+public class Observatory {
+    public static final int OBSERVATORY_RADIUS = 50;
+
+    private City city;
+    private final World world;
+    private final int id;
+    private final Location center;
+
+    public Observatory(World world, int id, int city_id, Location center) { //TODO needs the city too, doesn't it?
+        this.world = world;
+        this.id = id;
+        this.city = City.get(city_id);
+        this.center = center;
+    }
+
+    public City getCity(){
+        return city;
+    }
+
+    public World getWorld(){
+        return world;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public Location getCenter(){
+        return center;
+    }
+}
