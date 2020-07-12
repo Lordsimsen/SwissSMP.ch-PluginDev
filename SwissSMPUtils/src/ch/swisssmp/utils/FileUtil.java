1827254d913157a7c37839212396732a3e624cfe
detig_iii <star.etoa@hotmail.com>
2020-07-12 17:47:57 +0200

    Formatierung verbessert

--- SwissSMPUtils/src/ch/swisssmp/utils/FileUtil.java
+++ SwissSMPUtils/src/ch/swisssmp/utils/FileUtil.java
@@ -7,39 +7,39 @@ import java.util.Collection;
 import org.bukkit.craftbukkit.libs.org.apache.commons.io.FileUtils;
 
 public class FileUtil {
-	public static boolean deleteRecursive(File path) {
-	      if(path.exists()) {
-	          File files[] = path.listFiles();
-	          for(int i=0; i<files.length; i++) {
-	              if(files[i].isDirectory()) {
-	            	  deleteRecursive(files[i]);
-	              } else {
-	            	  try{
-	            		  files[i].delete();
-	            	  }
-	            	  catch(Exception e){
-	            		  e.printStackTrace();
-	            	  }
-	              }
-	          }
-	      }
-	      return(path.delete());
-		}
-		public static void copyDirectory(File source, File target){
-			copyDirectory(source, target, null);
-		}
-		public static void copyDirectory(File source, File target, Collection<String> ignoreFiles){
-			try {
-				if(ignoreFiles==null){
-					FileUtils.copyDirectory(source, target);
-				}
-				else{
-					FileUtils.copyDirectory(source, target, (File file)->{
-						return !ignoreFiles.contains(file.getName());
-					});
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+    public static boolean deleteRecursive(File path) {
+        if (path.exists()) {
+            File files[] = path.listFiles();
+            for (int i = 0; i < files.length; i++) {
+                if (files[i].isDirectory()) {
+                    deleteRecursive(files[i]);
+                } else {
+                    try {
+                        files[i].delete();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return (path.delete());
+    }
+
+    public static void copyDirectory(File source, File target) {
+        copyDirectory(source, target, null);
+    }
+
+    public static void copyDirectory(File source, File target, Collection<String> ignoreFiles) {
+        try {
+            if (ignoreFiles == null) {
+                FileUtils.copyDirectory(source, target);
+            } else {
+                FileUtils.copyDirectory(source, target, (File file) -> {
+                    return !ignoreFiles.contains(file.getName());
+                });
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
