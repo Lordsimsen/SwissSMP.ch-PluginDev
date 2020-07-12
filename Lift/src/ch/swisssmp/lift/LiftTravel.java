ed0682010a635f0a129f65965563a500003564c4
detig_iii <star.etoa@hotmail.com>
2020-07-12 17:47:46 +0200

    Effekte verbessert

--- Lift/src/ch/swisssmp/lift/LiftTravel.java
+++ Lift/src/ch/swisssmp/lift/LiftTravel.java
@@ -5,14 +5,10 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 import ch.swisssmp.lift.effect.LiftTravelEffect;
-import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.Sound;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Entity;
@@ -32,246 +28,245 @@ import ch.swisssmp.lift.event.LiftExitEvent;
 import ch.swisssmp.utils.SwissSMPler;
 
 public class LiftTravel implements Runnable, Listener {
-	private final static HashMap<LiftInstance,LiftTravel> travels = new HashMap<LiftInstance,LiftTravel>();
+    private final static HashMap<LiftInstance, LiftTravel> travels = new HashMap<LiftInstance, LiftTravel>();
 
-	private final World world;
-	private final LiftInstance lift;
-	private final LiftFloor from;
-	private final LiftFloor to;
-	
-	private final double fromY;
-	private final double toY;
-	
-	private long t;
-	private double height;
-	private double direction;
-	private long duration;
-	private Vector speed;
-	private LiftTravelEffect effect;
-	
-	private BukkitTask task;
-	
-	private final List<Entity> entities = new ArrayList<Entity>();
-	private HashMap<Block,BlockData> originalData;
-	
-	private LiftTravel(LiftFloor from, LiftFloor to){
-		this.world = from.getLift().getWorld();
-		this.lift = from.getLift();
-		this.from = from;
-		this.to = to;
-		this.fromY = from.getY()+1;
-		this.toY = to.getY()+1.5;
-	}
-	
-	private void initialize(){
-		height = Math.abs(fromY - toY);
-		LiftType type = lift.getType();
-		long timeInSeconds = Math.max(1, Math.round(height / type.getSpeed()));
-		t = 0;
-		direction = fromY <= toY ? 1 : -1;
-		duration = timeInSeconds*20;
-		speed = new Vector(0, type.getSpeed() * direction / 20, 0);
-		effect = type.getTravelEffect();
-		originalData = this.clearShaft(this.lift);
-		task = Bukkit.getScheduler().runTaskTimer(LiftPlugin.getInstance(), this, 0, 1);
-		Bukkit.getPluginManager().registerEvents(this, LiftPlugin.getInstance());
-		travels.put(from.getLift(), this);
-	}
-	
-	@Override
-	public void run() {
-		t++;
-		double progress = (double) t / duration;
-		double y = fromY + height * progress * direction;
-		this.updatePassengers(y);
-		for(Entity entity : entities){
-			if(entity.getVehicle()!=null) continue;
-			Location location = entity.getLocation();
-			double delta = y-location.getY();
-			if(Math.abs(delta)>4){
-				location.setY(y);
-				teleportEntityWithPassengers(entity,location);
-			}
-			Vector velocity = new Vector(0,speed.getY()+delta/20,0);
-			entity.setVelocity(velocity);
-			entity.setFallDistance(0);
-			if(effect!=null) effect.play(entity,velocity, t);
-		}
-		if(t>=duration){
-			complete();
-		}
-	}
-	
-	@EventHandler
-	private void onEntityDeath(EntityDeathEvent event){
-		if(!this.entities.contains(event.getEntity())) return;
-		removePassenger(event.getEntity());
-	}
-	
-	@EventHandler
-	private void onPlayerQuit(PlayerQuitEvent event){
-		if(!this.entities.contains(event.getPlayer())) return;
-		removePassenger(event.getPlayer());
-	}
-	
-	@EventHandler
-	private void onPlayerToggleFlight(PlayerToggleFlightEvent event){
-		if(entities.contains(event.getPlayer())) event.setCancelled(true);
-	}
-	
-	private void teleportEntityWithPassengers(Entity entity, Location location){
-		List<Entity> passengers = entity.getPassengers();
-		for(Entity passenger : passengers){
-			entity.removePassenger(passenger);
-		}
-		entity.teleport(location);
-		for(Entity passenger : passengers){
-			entity.addPassenger(passenger);
-		}
-	}
-	
-	private void complete(){
-		if(direction>0){
-			List<Entity> entities = new ArrayList<Entity>(this.entities);
-			teleportEntitiesToDestination(entities);
-			Bukkit.getScheduler().runTaskLater(LiftPlugin.getInstance(), ()->{
-				teleportEntitiesToDestination(entities);
-			}, 1l);
-		}
-		for(Entity entity : entities){
-			if(!(entity instanceof Player)) continue;
-			Player player = (Player) entity;
-			player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_CHIME, 4, 1.5f);
-			SwissSMPler.get(player).sendActionBar(to.getName());
-		}
-		finish();
-	}
-	
-	private void teleportEntitiesToDestination(List<Entity> entities){
-		double targetY = toY;
-		for(Entity entity : entities){
-			if(entity.getVehicle()!=null) continue;
-			entity.setVelocity(new Vector());
-			Location location = entity.getLocation();
-			double safetyGap = entity instanceof Player ? 0 : (entity.getPassengers().size()>0 ? 1 : 0.5);
-			if(location.getY()<targetY+safetyGap){
-				location.setY(targetY+safetyGap);
-				teleportEntityWithPassengers(entity,location);
-			}
-		}
-	}
-	
-	public void cancel(){
-		finish();
-	}
-	
-	private void finish(){
-		for(Entity entity : new ArrayList<Entity>(entities)){
-			this.removePassenger(entity);
-		}
-		this.resetShaft(originalData);
-		if(task!=null) task.cancel();
-		travels.remove(lift);
-		HandlerList.unregisterAll(this);
-	}
-	
-	private HashMap<Block,BlockData> clearShaft(LiftInstance instance){
-		HashMap<Block,BlockData> result = new HashMap<Block,BlockData>();
-		int lowerY = Math.min(from.getY(), to.getY());
-		int upperY = Math.max(from.getY(), to.getY());
-		for(LiftFloor floor : instance.getIntermediateFloors()){
-			if(floor.getY()<=lowerY || floor.getY() > upperY) continue;
-			for(Block block : floor.getBlocks()){
-				BlockData data = block.getBlockData().clone();
-				result.put(block, data);
-				block.setType(Material.AIR);
-			}
-		}
-		return result;
-	}
-	
-	private void resetShaft(HashMap<Block,BlockData> original){
-		for(Entry<Block,BlockData> entry : original.entrySet()){
-			Block block = entry.getKey();
-			BlockData data = entry.getValue();
-			block.setBlockData(data);
-		}
-	}
-	
-	private void updatePassengers(double y){
-		Collection<Entity> entities = this.getEntities(world, lift);
-		for(Entity entity : entities){
-			if(this.entities.contains(entity) || !entity.hasGravity()) continue;
-			if(Math.abs(entity.getLocation().getY() - y)>5) continue;
-			addPassenger(entity);
-		}
-		Collection<Entity> leftEntities = new ArrayList<Entity>(3);
-		for(Entity entity : this.entities){
-			if(entities.contains(entity)) continue;
-			leftEntities.add(entity);
-		}
-		for(Entity entity : leftEntities){
-			removePassenger(entity);
-		}
-		if(this.entities.size()==0) this.cancel();
-	}
-	
-	private boolean addPassenger(Entity entity){
-		LiftEnterEvent event = new LiftEnterEvent(entity, this);
-		try{
-			Bukkit.getPluginManager().callEvent(event);
-		}
-		catch(Exception e){
-			Debug.Log(e);
-		}
-		if(event.isCancelled()) return false;
-		if(entity instanceof Player){
-			Player player = (Player) entity;
-			player.setAllowFlight(true);
-			player.setFlying(true);
-			player.setAllowFlight(false);
-		}
-		entity.setGravity(false);
-		this.entities.add(entity);
-		return true;
-	}
-	
-	private void removePassenger(Entity entity){
-		this.entities.remove(entity);
-		if(entity instanceof Player){
-			Player player = (Player) entity;
-			player.setFlying(false);
-			player.setAllowFlight(player.getGameMode()!=GameMode.ADVENTURE && player.getGameMode()!=GameMode.SURVIVAL);
-		}
-		entity.setGravity(true);
-		entity.setVelocity(new Vector());
-		LiftExitEvent event = new LiftExitEvent(entity, this);
-		try{
-			Bukkit.getPluginManager().callEvent(event);
-		}
-		catch(Exception e){
-			Debug.Log(e);
-		}
-	}
-	
-	private Collection<Entity> getEntities(World world, LiftInstance lift){
-		return world.getNearbyEntities(lift.getBoundingBox(), e->e instanceof LivingEntity);
-	}
+    private final World world;
+    private final LiftInstance lift;
+    private final LiftFloor from;
+    private final LiftFloor to;
 
-	protected static void cancelAll(){
-		for(LiftTravel travel : travels.values()){
-			travel.cancel();
-		}
-	}
-	
-	public static LiftTravel get(LiftInstance instance){
-		return travels.get(instance);
-	}
-	
-	public static LiftTravel start(LiftFloor from, LiftFloor to){
-		if(travels.containsKey(from.getLift())) return null;
-		LiftTravel result = new LiftTravel(from, to);
-		result.initialize();
-		return result;
-	}
+    private final double fromY;
+    private final double toY;
+
+    private long t;
+    private double height;
+    private double direction;
+    private long duration;
+    private Vector speed;
+    private LiftTravelEffect effect;
+
+    private BukkitTask task;
+
+    private final List<Entity> entities = new ArrayList<Entity>();
+    private HashMap<Block, BlockData> originalData;
+
+    private LiftTravel(LiftFloor from, LiftFloor to) {
+        this.world = from.getLift().getWorld();
+        this.lift = from.getLift();
+        this.from = from;
+        this.to = to;
+        this.fromY = from.getY() + 1;
+        this.toY = to.getY() + 1.5;
+    }
+
+    private void initialize() {
+        height = Math.abs(fromY - toY);
+        LiftType type = lift.getType();
+        long timeInSeconds = Math.max(1, Math.round(height / type.getSpeed()));
+        t = 0;
+        direction = fromY <= toY ? 1 : -1;
+        duration = timeInSeconds * 20;
+        speed = new Vector(0, type.getSpeed() * direction / 20, 0);
+        effect = type.getTravelEffect();
+        originalData = this.clearShaft(this.lift);
+        task = Bukkit.getScheduler().runTaskTimer(LiftPlugin.getInstance(), this, 0, 1);
+        Bukkit.getPluginManager().registerEvents(this, LiftPlugin.getInstance());
+        travels.put(from.getLift(), this);
+    }
+
+    @Override
+    public void run() {
+        t++;
+        double progress = (double) t / duration;
+        double y = fromY + height * progress * direction;
+        this.updatePassengers(y);
+        for (Entity entity : entities) {
+            if (entity.getVehicle() != null) continue;
+            Location location = entity.getLocation();
+            double delta = y - location.getY();
+            if (Math.abs(delta) > 4) {
+                location.setY(y);
+                teleportEntityWithPassengers(entity, location);
+            }
+            Vector velocity = new Vector(0, speed.getY() + delta / 20, 0);
+            entity.setVelocity(velocity);
+            entity.setFallDistance(0);
+            if (effect != null) effect.play(entity, velocity, t);
+        }
+        if (t >= duration) {
+            complete();
+        }
+    }
+
+    @EventHandler
+    private void onEntityDeath(EntityDeathEvent event) {
+        if (!this.entities.contains(event.getEntity())) return;
+        removePassenger(event.getEntity());
+    }
+
+    @EventHandler
+    private void onPlayerQuit(PlayerQuitEvent event) {
+        if (!this.entities.contains(event.getPlayer())) return;
+        removePassenger(event.getPlayer());
+    }
+
+    @EventHandler
+    private void onPlayerToggleFlight(PlayerToggleFlightEvent event) {
+        if (entities.contains(event.getPlayer())) event.setCancelled(true);
+    }
+
+    private void teleportEntityWithPassengers(Entity entity, Location location) {
+        List<Entity> passengers = entity.getPassengers();
+        for (Entity passenger : passengers) {
+            entity.removePassenger(passenger);
+        }
+        entity.teleport(location);
+        for (Entity passenger : passengers) {
+            entity.addPassenger(passenger);
+        }
+    }
+
+    private void complete() {
+        if (direction > 0) {
+            List<Entity> entities = new ArrayList<Entity>(this.entities);
+            teleportEntitiesToDestination(entities);
+            Bukkit.getScheduler().runTaskLater(LiftPlugin.getInstance(), () -> {
+                teleportEntitiesToDestination(entities);
+            }, 1l);
+        }
+        for (Entity entity : entities) {
+            if (!(entity instanceof Player)) continue;
+            Player player = (Player) entity;
+            if (effect != null) effect.playEnd(entity);
+            SwissSMPler.get(player).sendActionBar(to.getName());
+        }
+        finish();
+    }
+
+    private void teleportEntitiesToDestination(List<Entity> entities) {
+        double targetY = toY;
+        for (Entity entity : entities) {
+            if (entity.getVehicle() != null) continue;
+            entity.setVelocity(new Vector());
+            Location location = entity.getLocation();
+            double safetyGap = entity instanceof Player ? 0 : (entity.getPassengers().size() > 0 ? 1 : 0.5);
+            if (location.getY() < targetY + safetyGap) {
+                location.setY(targetY + safetyGap);
+                teleportEntityWithPassengers(entity, location);
+            }
+        }
+    }
+
+    public void cancel() {
+        finish();
+    }
+
+    private void finish() {
+        for (Entity entity : new ArrayList<Entity>(entities)) {
+            this.removePassenger(entity);
+        }
+        this.resetShaft(originalData);
+        if (task != null) task.cancel();
+        travels.remove(lift);
+        HandlerList.unregisterAll(this);
+    }
+
+    private HashMap<Block, BlockData> clearShaft(LiftInstance instance) {
+        HashMap<Block, BlockData> result = new HashMap<Block, BlockData>();
+        int lowerY = Math.min(from.getY(), to.getY());
+        int upperY = Math.max(from.getY(), to.getY());
+        for (LiftFloor floor : instance.getIntermediateFloors()) {
+            if (floor.getY() <= lowerY || floor.getY() > upperY) continue;
+            for (Block block : floor.getBlocks()) {
+                BlockData data = block.getBlockData().clone();
+                result.put(block, data);
+                block.setType(Material.AIR);
+            }
+        }
+        return result;
+    }
+
+    private void resetShaft(HashMap<Block, BlockData> original) {
+        for (Entry<Block, BlockData> entry : original.entrySet()) {
+            Block block = entry.getKey();
+            BlockData data = entry.getValue();
+            block.setBlockData(data);
+        }
+    }
+
+    private void updatePassengers(double y) {
+        Collection<Entity> entities = this.getEntities(world, lift);
+        for (Entity entity : entities) {
+            if (this.entities.contains(entity) || !entity.hasGravity()) continue;
+            if (Math.abs(entity.getLocation().getY() - y) > 5) continue;
+            addPassenger(entity);
+        }
+        Collection<Entity> leftEntities = new ArrayList<Entity>(3);
+        for (Entity entity : this.entities) {
+            if (entities.contains(entity)) continue;
+            leftEntities.add(entity);
+        }
+        for (Entity entity : leftEntities) {
+            removePassenger(entity);
+        }
+        if (this.entities.size() == 0) this.cancel();
+    }
+
+    private boolean addPassenger(Entity entity) {
+        LiftEnterEvent event = new LiftEnterEvent(entity, this);
+        try {
+            Bukkit.getPluginManager().callEvent(event);
+        } catch (Exception e) {
+            Debug.Log(e);
+        }
+        if (event.isCancelled()) return false;
+        if (entity instanceof Player) {
+            Player player = (Player) entity;
+            player.setAllowFlight(true);
+            player.setFlying(true);
+            player.setAllowFlight(false);
+        }
+        entity.setGravity(false);
+        if (effect != null) effect.playStart(entity);
+        this.entities.add(entity);
+        return true;
+    }
+
+    private void removePassenger(Entity entity) {
+        this.entities.remove(entity);
+        if (entity instanceof Player) {
+            Player player = (Player) entity;
+            player.setFlying(false);
+            player.setAllowFlight(player.getGameMode() != GameMode.ADVENTURE && player.getGameMode() != GameMode.SURVIVAL);
+        }
+        entity.setGravity(true);
+        entity.setVelocity(new Vector());
+        LiftExitEvent event = new LiftExitEvent(entity, this);
+        try {
+            Bukkit.getPluginManager().callEvent(event);
+        } catch (Exception e) {
+            Debug.Log(e);
+        }
+    }
+
+    private Collection<Entity> getEntities(World world, LiftInstance lift) {
+        return world.getNearbyEntities(lift.getBoundingBox(), e -> e instanceof LivingEntity);
+    }
+
+    protected static void cancelAll() {
+        for (LiftTravel travel : travels.values()) {
+            travel.cancel();
+        }
+    }
+
+    public static LiftTravel get(LiftInstance instance) {
+        return travels.get(instance);
+    }
+
+    public static LiftTravel start(LiftFloor from, LiftFloor to) {
+        if (travels.containsKey(from.getLift())) return null;
+        LiftTravel result = new LiftTravel(from, to);
+        result.initialize();
+        return result;
+    }
 }
--- Lift/src/ch/swisssmp/lift/effect/LiftTravelEffect.java
+++ Lift/src/ch/swisssmp/lift/effect/LiftTravelEffect.java
@@ -1,31 +1,86 @@
 package ch.swisssmp.lift.effect;
 
 import ch.swisssmp.utils.Random;
-import org.bukkit.Color;
-import org.bukkit.Location;
-import org.bukkit.Particle;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
 public class LiftTravelEffect {
 
     public static final LiftTravelEffect NONE = new LiftTravelEffect();
-    public static final LiftTravelEffect NETHERITE = new LiftTravelEffect(new Particle.DustOptions(Color.fromRGB(50, 20, 10), 1));
+    public static final LiftTravelEffect NETHERITE = new LiftTravelEffect(
+            new Particle.DustOptions(Color.fromRGB(50, 20, 10), 1),
+            Sound.BLOCK_BUBBLE_COLUMN_WHIRLPOOL_INSIDE,
+            0.8f,
+            Sound.BLOCK_ANVIL_LAND,
+            0.8f);
 
     private static final int INTERVAL = 1;
     private static final int AMOUNT = 3;
     private static final float PARTICLE_RADIUS = 0.6f;
     private static final Random random = new Random();
 
+    private final String startSoundId;
+    private final Sound startSound;
+    private final float startSoundPitch;
+    private final String endSoundId;
+    private final Sound endSound;
+    private final float endSoundPitch;
     private final Particle.DustOptions dust;
 
     private LiftTravelEffect() {
         dust = null;
+        startSoundId = null;
+        startSound = null;
+        startSoundPitch = 1;
+        endSoundId = null;
+        endSound = Sound.BLOCK_NOTE_BLOCK_CHIME;
+        endSoundPitch = 1.5f;
     }
 
-    private LiftTravelEffect(Particle.DustOptions dust) {
+    private LiftTravelEffect(Particle.DustOptions dust, Sound startSound, float startSoundPitch, Sound endSound, float endSoundPitch) {
         this.dust = dust;
+        this.startSoundId = null;
+        this.startSound = startSound;
+        this.startSoundPitch = startSoundPitch;
+        this.endSoundId = null;
+        this.endSound = endSound;
+        this.endSoundPitch = endSoundPitch;
+    }
+
+    private LiftTravelEffect(Particle.DustOptions dust, String startSoundId, float startSoundPitch, String endSoundId, float endSoundPitch) {
+        this.dust = dust;
+        this.startSoundId = startSoundId;
+        this.startSound = null;
+        this.startSoundPitch = startSoundPitch;
+        this.endSoundId = endSoundId;
+        this.endSound = null;
+        this.endSoundPitch = endSoundPitch;
+    }
+
+    public void playStart(Entity entity){
+        if(entity instanceof Player){
+            Player player = (Player) entity;
+            if(startSound!=null){
+                player.playSound(player.getLocation(), startSound, SoundCategory.BLOCKS, 16, startSoundPitch);
+            }
+            if(startSoundId!=null){
+                player.playSound(player.getLocation(), startSoundId, SoundCategory.BLOCKS, 16, startSoundPitch);
+            }
+        }
+    }
+
+    public void playEnd(Entity entity){
+        if(entity instanceof Player){
+            Player player = (Player) entity;
+            if(endSound!=null) {
+                player.playSound(player.getLocation().add(0,4,0), endSound, SoundCategory.BLOCKS, 4, endSoundPitch);
+            }
+            if(endSoundId!=null) {
+                player.playSound(player.getLocation().add(0,4,0), endSoundId, SoundCategory.BLOCKS, 4, endSoundPitch);
+            }
+        }
     }
 
     public void play(Entity entity, Vector motion, long tick) {
