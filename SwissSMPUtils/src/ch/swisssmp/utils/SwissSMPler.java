ae5e7842bc2a1a8357445c8bfcb7e05b2703e74e
detig_iii <star.etoa@hotmail.com>
2020-08-10 02:44:14 +0200

    null-Sicherheit zu SwissSMPler hinzugefügt

--- SwissSMPUtils/src/ch/swisssmp/utils/SwissSMPler.java
+++ SwissSMPUtils/src/ch/swisssmp/utils/SwissSMPler.java
@@ -5,7 +5,6 @@
  */
 package ch.swisssmp.utils;
 
-import java.util.HashMap;
 import java.util.UUID;
 
 import com.google.gson.JsonParser;
@@ -14,117 +13,127 @@ import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TextComponent;
 
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitTask;
-import org.bukkit.util.Vector;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.World;
 
 public final class SwissSMPler {
-	protected static HashMap<UUID, Vector> last_vectors = new HashMap<UUID, Vector>();
-	protected static HashMap<UUID, BukkitTask> afk_tasks = new HashMap<UUID, BukkitTask>();
-    protected final Player player;
-    private SwissSMPler(Player player){
-        this.player = player;
-    }
-    public static SwissSMPler get(Player player){
-        return new SwissSMPler(player);
-    }
-    public static SwissSMPler get(UUID player_uuid){
-        Player bukkitPlayer = Bukkit.getPlayer(player_uuid);
-        if(bukkitPlayer==null) return null;
-        return new SwissSMPler(bukkitPlayer);
-    }
-    
-    public void teleport(Location to){
-        player.teleport(to);
-    }
-    
-    public void sendMessage(String message){
-        player.sendMessage(message);
-    }
-    
-    public UUID getUniqueId()
-    {
-        return player.getUniqueId();
-    }
-    
-    public String getName()
-    {
-        return player.getName();
-    }
-    
-    public String getDisplayName()
-    {
-        return player.getDisplayName();
-    }
-    
-    public GameMode getGameMode(){
-    	return player.getGameMode();
-    }
-    
-    public int getLevel(){
-    	return player.getLevel();
-    }
-    
-    public float getExp(){
-    	return player.getExp();
-    }
-    
-    public void giveExp(int exp){
-    	player.giveExp(exp);
-    }
-    
-    public void setLevel(int level){
-    	player.setLevel(level);
-    }
-    
-    public void setExp(float exp){
-    	player.setExp(exp);
-    }
-    
-    public boolean hasPermission(String permission){
-    	return player.hasPermission(permission);
-    }
-    
-    public void setInvulnerable(boolean invulnerable){
-    	player.setInvulnerable(invulnerable);
-    }
-    
-    public Location getLocation(){
-    	return player.getLocation();
-    }
-    
-    public World getWorld(){
-    	return player.getWorld();
+
+    private final UUID playerUid;
+    private final Player player;
+
+    private SwissSMPler(UUID playerUid) {
+        this.playerUid = playerUid;
+        this.player = null;
     }
 
-    public void sendRawMessage(String rawMessage){
-        if(!player.isOnline())return;
+    private SwissSMPler(Player player) {
+        this.playerUid = player != null ? player.getUniqueId() : null;
+        this.player = player;
+    }
+
+    public static SwissSMPler get(Player player) {
+        return new SwissSMPler(player);
+    }
+
+    public static SwissSMPler get(UUID player_uuid) {
+        Player bukkitPlayer = Bukkit.getPlayer(player_uuid);
+        return bukkitPlayer != null ? new SwissSMPler(bukkitPlayer) : new SwissSMPler(player_uuid);
+    }
+
+    public void teleport(Location to) {
+        if (player == null) return;
+        player.teleport(to);
+    }
+
+    public void sendMessage(String message) {
+        if (player == null) return;
+        player.sendMessage(message);
+    }
+
+    public UUID getUniqueId() {
+        return playerUid;
+    }
+
+    public String getName() {
+        return player != null ? player.getName() : null;
+    }
+
+    public String getDisplayName() {
+        return player != null ? player.getDisplayName() : null;
+    }
+
+    public GameMode getGameMode() {
+        return player != null ? player.getGameMode() : null;
+    }
+
+    public int getLevel() {
+        return player != null ? player.getLevel() : null;
+    }
+
+    public float getExp() {
+        return player != null ? player.getExp() : null;
+    }
+
+    public void giveExp(int exp) {
+
+        if (player == null) return;
+        player.giveExp(exp);
+    }
+
+    public void setLevel(int level) {
+
+        if (player == null) return;
+        player.setLevel(level);
+    }
+
+    public void setExp(float exp) {
+        if (player == null) return;
+        player.setExp(exp);
+    }
+
+    public boolean hasPermission(String permission) {
+        return player != null && player.hasPermission(permission);
+    }
+
+    public void setInvulnerable(boolean invulnerable) {
+        if (player == null) return;
+        player.setInvulnerable(invulnerable);
+    }
+
+    public Location getLocation() {
+        return player != null ? player.getLocation() : null;
+    }
+
+    public World getWorld() {
+        return player != null ? player.getWorld() : null;
+    }
+
+    public void sendRawMessage(String rawMessage) {
+        if (player == null || !player.isOnline()) return;
         try {
             JsonParser parser = new JsonParser();
             parser.parse(rawMessage);
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "tellraw "+player.getName()+" "+rawMessage);
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "tellraw " + player.getName() + " " + rawMessage);
         } catch (Exception e) {
-            Bukkit.getLogger().warning("Tried to send invalid raw message:\n"+rawMessage);
+            Bukkit.getLogger().warning("Tried to send invalid raw message:\n" + rawMessage);
         }
     }
 
-    public void sendRawMessage(BaseComponent... components){
-    	if(!player.isOnline()) return;
+    public void sendRawMessage(BaseComponent... components) {
+        if (player == null || !player.isOnline()) return;
         player.spigot().sendMessage(net.md_5.bungee.api.ChatMessageType.CHAT, components);
-    } 
-    
-    public void sendActionBar(String message){
-    	if(player==null || message==null) return;
+    }
+
+    public void sendActionBar(String message) {
+        if (player == null || message == null) return;
         player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(message));
     }
-    
+
     public void sendTitle(String title, String subtitle) {
-    	if(!player.isOnline()) return;
+        if (player == null || !player.isOnline()) return;
         player.sendTitle(title, subtitle, 10, 70, 20);
     }
 }
